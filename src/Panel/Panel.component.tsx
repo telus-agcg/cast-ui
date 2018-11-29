e7b433c0511729d9c246462b584903ec205ad45a
ben-dalton <benadalton@gmail.com>
2018-11-29 10:01:06 -0500

    fix lint issues and adjust rule

--- src/Panel/Panel.component.tsx
+++ src/Panel/Panel.component.tsx
@@ -107,14 +107,14 @@ function collapseSection(element: HTMLElement) {
   // on the next frame (as soon as the previous style change has taken effect),
   // explicitly set the element's height to its current pixel height, so we
   // aren't transitioning out of 'auto'
-  requestAnimationFrame(function () {
-    element.style.height = sectionHeight + 'px';
+  requestAnimationFrame(() => {
+    element.style.height = `${sectionHeight}px`;
     element.style.transition = elementTransition;
 
     // on the next frame (as soon as the previous style change has taken effect),
     // have the element transition to height: 0
-    requestAnimationFrame(function () {
-      element.style.height = 0 + 'px';
+    requestAnimationFrame(() => {
+      element.style.height = '0px';
     });
   });
 }
@@ -124,7 +124,7 @@ function expandSection(element: HTMLElement) {
   const sectionHeight = element.scrollHeight;
 
   // have the element transition to the height of its inner content
-  element.style.height = sectionHeight + 'px';
+  element.style.height = `${sectionHeight}px`;
 
   // when the next css transition finishes (which should be the one we just triggered)
   element.addEventListener('transitionend', function (e) {
