a8e267804650a81ef530d16ec88fcbbe45b39992
Nick D'Alonzo <ndalonzo@xsinc.com>
2018-12-20 10:01:14 -0600

    FIx quotes, remove extra imports

--- src/Panel/Panel.component.tsx
+++ src/Panel/Panel.component.tsx
@@ -1,5 +1,5 @@
-import * as React from "react";
-import styled from "styled-components";
+import * as React from 'react';
+import styled from 'styled-components';
 
 type Props = {
   /** the content of the panel  */
@@ -28,7 +28,7 @@ type Props = {
 const PanelWrapper = styled.div`
   border: ${(props: Props) =>
     `${props.theme.borders.width} solid ${props.theme.styles[props.panelStyle]
-      .borderColor || "#eee"}`};
+      .borderColor || '#eee'}`};
   overflow: hidden;
   font-family: ${(props: Props) => props.theme.typography.fontFamily};
   border-radius: ${(props: Props) => props.theme.borders.radius};
@@ -45,15 +45,15 @@ const PanelHeader = styled.div`
     background: ${(props: Props) =>
       props.collapsible
         ? props.theme.styles[props.panelStyle].hoverLightFlood
-        : "auto"};
-    cursor: ${(props: Props) => (props.collapsible ? "pointer" : "auto")};
+        : 'auto'};
+    cursor: ${(props: Props) => (props.collapsible ? 'pointer' : 'auto')};
   }
 `;
 const PanelBody = styled.div`
   background: white;
   overflow: hidden;
   height: auto;
-  padding: ${(props: Props) => "5px 8px"};
+  padding: ${(props: Props) => '5px 8px'};
   font-size: 16px;
   opacity: ${(props: Props) => 1};
   transition: all 300ms ease-in-out;
@@ -104,7 +104,7 @@ export class Panel extends React.Component<Props, State> {
     return (
       <PanelWrapper panelStyle={this.props.panelStyle}>
         <PanelHeader
-          panelStyle={this.props.panelStyle || "default"}
+          panelStyle={this.props.panelStyle || 'default'}
           collapsible={this.props.collapsible}
           onClick={() => {
             this.toggleItem();
@@ -113,7 +113,7 @@ export class Panel extends React.Component<Props, State> {
           {this.props.title} {this.props.isCollapsed}
         </PanelHeader>
         <PanelBody
-          panelStyle={this.props.panelStyle || "default"}
+          panelStyle={this.props.panelStyle || 'default'}
           isCollapsed={this.localIsCollapsed}
           ref={this.bodyRef}
         >
@@ -128,14 +128,14 @@ const collapseSection = (element: HTMLElement | null) => {
   if (element !== null) {
     const sectionHeight = element.scrollHeight;
     const elementTransition = element.style.transition;
-    element.style.transition = "";
+    element.style.transition = '';
     requestAnimationFrame(() => {
       element.style.height = `${sectionHeight}px`;
       element.style.transition = elementTransition;
       requestAnimationFrame(() => {
-        element.style.height = "0px";
-        element.style.padding = "0 8px";
-        element.style.opacity = "0";
+        element.style.height = '0px';
+        element.style.padding = '0 8px';
+        element.style.opacity = '0';
       });
     });
   }
@@ -144,6 +144,6 @@ const collapseSection = (element: HTMLElement | null) => {
 const expandSection = (element: HTMLElement) => {
   const sectionHeight = element.scrollHeight;
   element.style.height = `${sectionHeight}px`;
-  element.style.padding = "5px 8px";
-  element.style.opacity = "1";
+  element.style.padding = '5px 8px';
+  element.style.opacity = '1';
 };
