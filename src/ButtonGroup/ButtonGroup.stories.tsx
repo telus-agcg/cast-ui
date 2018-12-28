44f3c2a52bf655fc1d0f442463486623d043c82b
Nick D'Alonzo <ndalonzo@xsinc.com>
2018-12-28 14:12:49 -0600

    WIP button group shell

---
+++ src/ButtonGroup/ButtonGroup.stories.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+import { wInfo } from '../storybook-utils';
+
+import { ButtonGroup } from './ButtonGroup.component';
+import { Button } from '../Button/Button.component';
+
+storiesOf('ButtonGroup', module).add(
+  'ButtonGroup',
+  wInfo(`
+  ### Notes
+  This is a ButtonGroup
+  ### Usage
+  ~~~js
+  <ButtonGroup />
+  ~~~`)(() => (
+    <ButtonGroup>
+        <Button
+        btnStyle="primary"
+        btnSize="md"
+        onClick={() => alert('OK')}
+        >
+            OK
+        </Button>
+        <Button
+        btnStyle="default"
+        btnSize="md"
+        onClick={() => alert('Cancel')}
+        >
+            Cancel
+        </Button>
+    </ButtonGroup>
+  )),
+);
