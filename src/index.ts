53c19f3586eebc2231d45aa3ad0776d24d5ba846
ndalonzo <42078450+ndalonzo@users.noreply.github.com>
2019-01-03 14:37:18 -0600

    Add component exports (#9)

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export * from './Button/Button.component';
+export * from './Input/Input.component';
+export * from './InputGroup/InputGroup.component';
+export * from './Panel/PanelHeader.component';
+export * from './Panel/Panel.component';
+export * from './Textarea/Textarea.component';
