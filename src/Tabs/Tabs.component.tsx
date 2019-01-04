53f1af7881df0dab6732b75b70688eeb112bf2c7
ndalonzo <42078450+ndalonzo@users.noreply.github.com>
2019-01-04 09:38:45 -0600

    Feature/tabs (#8)
    * WIP
    
    * Initial commit of wrapped react-tabs
    
    * Expose onSelect to wrapped tabs
    
    * Expose default tab index property.

---
+++ src/Tabs/Tabs.component.tsx
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import styled from 'styled-components';
+import { Tabs as ReactTabs } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+
+type PropsThemeOnly = {
+  /**
+   * From theme provider
+   *
+   * @default defaultTheme
+   **/
+  theme?: any;
+};
+
+type Props = PropsThemeOnly & {
+  /**
+  * Specify the tab that should be open on initial render.
+  * This is a zero-based index, so first tab is 0, second tab is 1, ...
+  *
+  * @default null
+  **/
+  defaultIndex?: number;
+  /**
+  * Function to fire when a tab page is selected
+  *
+  * @default 0
+  **/
+  onSelect?: any;
+};
+
+const STabWrapperDiv = styled.div`
+  font-family: ${(props: PropsThemeOnly) => props.theme.typography.fontFamily};
+`;
+
+export class Tabs extends React.Component<Props> {
+  public static readonly tabsRole: string = 'Tabs';
+
+  constructor(props: Props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <STabWrapperDiv>
+        <ReactTabs {...this.props} onSelect={this.props.onSelect}>
+          {this.props.children}
+        </ReactTabs>
+      </STabWrapperDiv>
+    );
+  }
+}
