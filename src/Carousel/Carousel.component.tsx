350a3b96ce4bf4169f0ec1b23b9199b557fb1235
ndalonzo <42078450+ndalonzo@users.noreply.github.com>
2019-01-23 09:03:19 -0600

    Initial commit of carousel component. (#18)

---
+++ src/Carousel/Carousel.component.tsx
@@ -0,0 +1,87 @@
+import * as React from 'react';
+import Slider from 'react-slick';
+
+import './Carousel.css';
+
+type Props = {
+  /**
+   * Specify if the carousel should automatically begin scrolling
+   *
+   * @default true
+   **/
+  autoplay?: boolean;
+  /**
+   * Specify how long each slide is shown (in milliseconds)
+   *
+   * @default 5000
+   **/
+  autoplaySpeed?: number;
+  /**
+   * Specify if left/right slide arrows should display
+   *
+   * @default true
+   **/
+  arrows?: boolean;
+  /**
+   * Specify if navigation dots should display
+   *
+   * @default true
+   **/
+  dots?: boolean;
+  /**
+   * Specify if scrolling should be infinite
+   *
+   * @default true
+   **/
+  infinite?: boolean;
+  /**
+   * Specify the number of slides to show at once
+   *
+   * @default 1
+   **/
+  slidesToShow?: number;
+  /**
+   * Specify the number of slides to scroll at one time
+   *
+   * @default 1
+   **/
+  slidesToScroll?: number;
+  /**
+   * Specify how long a slide should take to transition in/out
+   *
+   * @default 500
+   **/
+  transitionSpeed?: number;
+  /**
+   * From theme provider
+   *
+   * @default defaultTheme
+   **/
+  theme?: any;
+};
+
+class Carousel extends React.Component<Props> {
+  constructor(props: Props) {
+    super(props);
+  }
+
+  render() {
+    return (
+        <Slider
+          autoplay={(this.props.autoplay === undefined) ? true : this.props.autoplay}
+          autoplaySpeed={(this.props.autoplaySpeed === undefined) ? 5000 : this.props.autoplaySpeed}
+          arrows={(this.props.arrows === undefined) ? true : this.props.arrows}
+          dots={(this.props.dots === undefined) ? true : this.props.dots}
+          infinite={(this.props.infinite === undefined) ? true : this.props.infinite}
+          speed={(this.props.transitionSpeed === undefined) ? 500 : this.props.transitionSpeed}
+          slidesToShow={(this.props.slidesToShow === undefined) ? 1 : this.props.slidesToShow}
+          slidesToScroll={(this.props.slidesToScroll === undefined) ? 1 : this.props.slidesToScroll}
+          className="tkxs-carousel"
+          >
+          {this.props.children}
+        </Slider>
+    );
+  }
+}
+
+export default Carousel;
