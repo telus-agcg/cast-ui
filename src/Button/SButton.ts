6cbd5a0f9757fef55e7ca9f054e16bd3780dd3a8
Dantiff <20873103+Dantiff@users.noreply.github.com>
2019-02-04 22:47:03 +0300

    create a single function for calculating button border, background and text colors for different button states

--- src/Button/SButton.ts
+++ src/Button/SButton.ts
@@ -41,33 +41,164 @@ type Props = {
   theme?: any;
 };
 
+const computeColor: Function = (
+  btnState: string,
+  selected: Boolean,
+  outline: Boolean,
+  btnStyle: string,
+  theme: any,
+) => {
+  switch (true) {
+    case selected === true:
+      switch (btnState) {
+        case 'hover':
+          return {
+            background: theme.styles[btnStyle].hoverFlood,
+            color: theme.styles[btnStyle].reverseText,
+            borderColor: theme.styles[btnStyle].hoverFlood,
+          };
+        case 'disabled':
+          return {
+            background: theme.colors.disabledText,
+            color: theme.colors.white,
+            borderColor: theme.styles[btnStyle].disabledText,
+          };
+        default:
+          return {
+            background: theme.styles[btnStyle].selectedFlood,
+            color: theme.styles[btnStyle].reverseText,
+            borderColor: theme.styles[btnStyle].borderRadius,
+          };
+      }
+    case outline === true:
+      switch (btnState) {
+        case 'hover':
+          return {
+            background: theme.styles[btnStyle].reverseText,
+            color: theme.styles[btnStyle].hoverFlood,
+            borderColor: theme.styles[btnStyle].hoverFlood,
+          };
+        case 'disabled':
+          return {
+            background: theme.colors.white,
+            color: theme.colors.disabledText,
+            borderColor: theme.styles[btnStyle].disabledText,
+          };
+        default:
+          return {
+            background: theme.styles[btnStyle].reverseText,
+            color: theme.styles[btnStyle].flood,
+            borderColor: theme.styles[btnStyle].borderRadius,
+          };
+      }
+    default:
+      switch (btnState) {
+        case 'hover':
+          return {
+            background: theme.styles[btnStyle].hoverFlood,
+            color: theme.styles[btnStyle].reverseText,
+            borderColor: theme.styles[btnStyle].hoverFlood,
+          };
+        case 'disabled':
+          return {
+            background: theme.colors.disabledText,
+            color: theme.colors.white,
+            borderColor: theme.styles[btnStyle].disabledText,
+          };
+        default:
+          return {
+            background: theme.styles[btnStyle].flood,
+            color: theme.styles[btnStyle].reverseText,
+            borderColor: theme.styles[btnStyle].borderRadius,
+          };
+      }
+  }
+};
+
 const SButton = styled.button`
   min-width: 75px;
-  background: ${(props: Props) =>
-    props.selected
-      ? props.theme.styles[props.btnStyle].selectedFlood
-      : props.theme.styles[props.btnStyle].flood}
-  border: 1px solid ${(props: Props) =>
-    props.theme.styles[props.btnStyle].borderColor};
   border-radius: ${(props: Props) =>
-    props.theme.styles[props.btnStyle].borderRadius};
+    props.theme.common[props.btnSize].borderRadius};
+  background: ${(props: Props) =>
+    computeColor(
+      'normal',
+      props.selected,
+      props.outline,
+      props.btnStyle,
+      props.theme,
+    ).background}
+  border: 1px solid ${(props: Props) =>
+    computeColor(
+      'normal',
+      props.selected,
+      props.outline,
+      props.btnStyle,
+      props.theme,
+    ).borderColor}
   padding: ${(props: Props) => props.theme.common[props.btnSize].padding}
   font-family: ${(props: Props) => props.theme.typography.fontFamily};
   font-size: ${(props: Props) => props.theme.common[props.btnSize].fontSize}
   font-weight: 600;
-  color: ${(props: Props) => props.theme.styles[props.btnStyle].reverseText};
-  &:hover {
+  color: ${(props: Props) =>
+    computeColor(
+      'normal',
+      props.selected,
+      props.outline,
+      props.btnStyle,
+      props.theme,
+    ).color}
+  &:hover,&:active {
     background: ${(props: Props) =>
-      props.theme.styles[props.btnStyle].hoverFlood};
+      computeColor(
+        'hover',
+        props.selected,
+        props.outline,
+        props.btnStyle,
+        props.theme,
+      ).background};
+    color: ${(props: Props) =>
+      computeColor(
+        'hover',
+        props.selected,
+        props.outline,
+        props.btnStyle,
+        props.theme,
+      ).color};
     border: 1px solid ${(props: Props) =>
-      props.theme.styles[props.btnStyle].hoverFlood};
+      computeColor(
+        'hover',
+        props.selected,
+        props.outline,
+        props.btnStyle,
+        props.theme,
+      ).borderColor}
     cursor: pointer;
   }
   &:disabled {
     background: ${(props: Props) =>
-      props.theme.styles[props.btnStyle].hoverFlood}
+      computeColor(
+        'disabled',
+        props.selected,
+        props.outline,
+        props.btnStyle,
+        props.theme,
+      ).background}
+    color: ${(props: Props) =>
+      computeColor(
+        'disabled',
+        props.selected,
+        props.outline,
+        props.btnStyle,
+        props.theme,
+      ).color}
     border: 1px solid ${(props: Props) =>
-      props.theme.styles[props.btnStyle].hoverFlood}
+      computeColor(
+        'disabled',
+        props.selected,
+        props.outline,
+        props.btnStyle,
+        props.theme,
+      ).borderColor}
     cursor: not-allowed;
   }
 `;
