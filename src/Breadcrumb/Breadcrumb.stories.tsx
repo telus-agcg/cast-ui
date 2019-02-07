fc852719bdc0c56677efb05376ec679dda227032
Dantiff <20873103+Dantiff@users.noreply.github.com>
2019-02-08 01:38:11 +0300

    partially show sample of breadcrumbs

--- src/Breadcrumb/Breadcrumb.stories.tsx
+++ src/Breadcrumb/Breadcrumb.stories.tsx
@@ -1,40 +1,97 @@
 import * as React from 'react';
-// import ReactDOM from 'react-dom';
-// import { BrowserRouter, Route } from 'react-router-dom';
-// import { Router } from 'react-router';
-// import createBrowserHistory from 'history/createBrowserHistory';
+import { Router } from 'react-router';
+import createBrowserHistory from 'history/createBrowserHistory';
+import { Switch, NavLink } from 'react-router-dom';
 
 import { storiesOf } from '@storybook/react';
 // import { select } from '@storybook/addon-knobs/react';
 import { wInfo } from '../storybook-utils';
-// import { Breadcrumb, CrumbRoute } from './';
-import { Breadcrumbs } from './';
+import { Breadcrumbs, CrumbRoute } from './';
 
-// const newHistory = createBrowserHistory();
-// const render = (Root: any) => {
-//   ReactDOM.render(
-//     <BrowserRouter>
-//       <Router history={newHistory}>
-//         <CrumbRoute title="Index" path="/" component={Root} />
-//         <Route
-//           render={routeProps => (
-//             <Breadcrumb
-//               data={{
-//                 title: 'Home',
-//                 pathname: routeProps.match.url,
-//                 search: routeProps.location.search,
-//               }}>
-//               <div {...routeProps}> My Home Component </div>
-//             </Breadcrumb>
-//           )}
-//         />
-//       </Router>
-//     </BrowserRouter>,
-//     document.getElementById('root'),
-//   );
-// };
-// render(<div>My original Component</div>);
+const newHistory = createBrowserHistory();
+const Event = (props: any) => (
+  <div className="event">
+    <h3>{props.name}</h3>
+    <p>More information about the {props.name} here ...</p>
+  </div>
+);
 
+const CastUiButtons = (props: any) => (
+  <div className="event">
+    <h3>{props.name}</h3>
+    <p>More information about the {props.name}!</p>
+    <ul className="demo__links">
+      <li>
+        <NavLink to={`/buttons/standard-buttons`}>Standard Buttons</NavLink>
+      </li>
+      <li>
+        <NavLink to={`/buttons/outline-buttons`}>Outline Buttons</NavLink>
+      </li>
+    </ul>
+    <Switch>
+      <CrumbRoute
+        title="Cast UI Buttons"
+        path={`/buttons/standard-buttons`}
+        render={() => <Event name="Standard Buttons" />}
+      />
+      <CrumbRoute
+        title="Outline Buttons"
+        path={`/buttons/outline-buttons`}
+        render={() => <Event name="Outline Buttons" />}
+      />
+    </Switch>
+  </div>
+);
+
+storiesOf('Breadcrumb', module).add(
+  'Collective Usage',
+  wInfo(`
+
+  ### Notes
+
+  Illustrates collective usage of the breadcrumb components
+
+  ### Usage
+  ~~~js
+    <Route
+      {...props}
+      render={routeProps => (
+        <Breadcrumb
+          data={{
+            title: 'Home',
+            pathname: routeProps.match.url,
+            search: includeSearch ? routeProps.location.search : null,
+          }}>
+          <MyComponent {...routeProps} />
+        </Breadcrumb>
+      )}
+    />
+  ~~~`)(() => (
+    <div>
+      <Router history={newHistory}>
+        <div>
+          <Breadcrumbs
+            BreadcrumbsContainer="nav"
+            BreadcrumbItemContainer="span"
+            separator="span"
+          />
+          <ul className="demo__links">
+            <li>
+              <NavLink to={`/buttons`}>Cast UI Buttons</NavLink>
+            </li>
+          </ul>
+          <Switch>
+            <CrumbRoute
+              title="Cast UI Buttons"
+              path={`/buttons`}
+              render={() => <CastUiButtons name="Cast UI Buttons" />}
+            />
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  )),
+);
 storiesOf('Breadcrumb', module).add(
   'Breadcrumb',
   wInfo(`
@@ -60,7 +117,19 @@ storiesOf('Breadcrumb', module).add(
         </Breadcrumb>
       )}
     />
-  ~~~`)(() => <div>My Component</div>),
+  ~~~`)(() => (
+    <div>
+      <Router history={newHistory}>
+        <Switch>
+          <CrumbRoute
+            title="Breadcrumb Button"
+            path={`/buttons/outline-buttons`}
+            component={() => <Event name="Breadcrumb Button" />}
+          />
+        </Switch>
+      </Router>
+    </div>
+  )),
 );
 
 storiesOf('Breadcrumb', module).add(
@@ -80,7 +149,19 @@ storiesOf('Breadcrumb', module).add(
       path="/home"
       component={MyComponent}
     />
-  ~~~`)(() => <span />),
+  ~~~`)(() => (
+    <div>
+      <Router history={newHistory}>
+        <Switch>
+          <CrumbRoute
+            title="Breadcrumb Button"
+            path={`/buttons/outline-buttons`}
+            component={() => <Event name="Breadcrumb Button" />}
+          />
+        </Switch>
+      </Router>
+    </div>
+  )),
 );
 
 storiesOf('Breadcrumb', module).add(
