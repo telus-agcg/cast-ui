cd1f3f849e584973e355239c9a10669f2a0c2df7
Dantiff <20873103+Dantiff@users.noreply.github.com>
2019-02-07 14:48:46 +0300

    setup redux-based store for the crumbs

--- src/Breadcrumb/Breadcrumb.stories.tsx
+++ src/Breadcrumb/Breadcrumb.stories.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
-import { select } from '@storybook/addon-knobs/react';
+// import { select } from '@storybook/addon-knobs/react';
 
-import { BreadcrumbsItem } from './Breadcrumb.component';
+// import { BreadcrumbsItem } from './Breadcrumb.component';
 import { wInfo } from '../storybook-utils';
 
 storiesOf('Breadcrumb', module).add(
@@ -21,19 +21,20 @@ storiesOf('Breadcrumb', module).add(
       animationSpeed={2}
     />
   ~~~`)(() => (
-    <BreadcrumbsItem
-      color={select(
-        'color',
-        ['lightGray', 'gray', 'blue', 'white', 'red', 'yellow'],
-        'lightGray',
-      )}
-      size={select('size', [20, 30, 40, 50, 60], 40)}
-      animationSpeed={select('animationSpeed', [1, 2, 3, 4, 5], 2)}
-      transitionType={select(
-        'transitionType',
-        ['ease-in-out', 'ease-in', 'ease-out', 'linear', 'ease'],
-        'ease-in-out',
-      )}
-    />
+    <span>Comming</span>
+    // <BreadcrumbsItem
+    //   color={select(
+    //     'color',
+    //     ['lightGray', 'gray', 'blue', 'white', 'red', 'yellow'],
+    //     'lightGray',
+    //   )}
+    //   size={select('size', [20, 30, 40, 50, 60], 40)}
+    //   animationSpeed={select('animationSpeed', [1, 2, 3, 4, 5], 2)}
+    //   transitionType={select(
+    //     'transitionType',
+    //     ['ease-in-out', 'ease-in', 'ease-out', 'linear', 'ease'],
+    //     'ease-in-out',
+    //   )}
+    // />
   )),
 );
--- src/Panel/PanelHeader.component.tsx
+++ src/Panel/PanelHeader.component.tsx
@@ -59,8 +59,7 @@ const SExpandIcon = styled.div`
   padding: 0;
   margin: 4px 0 0;
   // tslint:disable-next-line
-  background: transparent
-    url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIzMiIgaGVpZ2h0PSIzMiIgdmlld0JveD0iMCAwIDMyIDMyIj48cGF0aCBmaWxsPSIjMzU3YmRmIiBkPSJNMTYuMDAzIDE4LjYyNmw3LjA4MS03LjA4MUwyNSAxMy40NmwtOC45OTcgOC45OTgtOS4wMDMtOSAxLjkxNy0xLjkxNnoiLz48L3N2Zz4=');
+  background: transparent url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIzMiIgaGVpZ2h0PSIzMiIgdmlld0JveD0iMCAwIDMyIDMyIj48cGF0aCBmaWxsPSIjMzU3YmRmIiBkPSJNMTYuMDAzIDE4LjYyNmw3LjA4MS03LjA4MUwyNSAxMy40NmwtOC45OTcgOC45OTgtOS4wMDMtOSAxLjkxNy0xLjkxNnoiLz48L3N2Zz4=');
   border: 0;
   -webkit-appearance: none;
   text-shadow: none;
@@ -78,8 +77,7 @@ const SCollapseIcon = styled.div`
   float: right;
   padding: 0;
   margin: 4px 0 0;
-  background: transparent
-    url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIzMiIgaGVpZ2h0PSIzMiIgdmlld0JveD0iMCAwIDMyIDMyIj48cGF0aCBmaWxsPSIjMzU3YmRmIiBkPSJNMTUuOTk3IDEzLjM3NGwtNy4wODEgNy4wODFMNyAxOC41NGw4Ljk5Ny04Ljk5OCA5LjAwMyA5LTEuOTE2IDEuOTE2eiIvPjwvc3ZnPg==');
+  background: transparent url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIzMiIgaGVpZ2h0PSIzMiIgdmlld0JveD0iMCAwIDMyIDMyIj48cGF0aCBmaWxsPSIjMzU3YmRmIiBkPSJNMTUuOTk3IDEzLjM3NGwtNy4wODEgNy4wODFMNyAxOC41NGw4Ljk5Ny04Ljk5OCA5LjAwMyA5LTEuOTE2IDEuOTE2eiIvPjwvc3ZnPg==');
   border: 0;
   -webkit-appearance: none;
   text-shadow: none;
