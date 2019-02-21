82d6ec4ddfdeb8a14e401ed9f749c6e8508dfa4b
Dantiff <20873103+Dantiff@users.noreply.github.com>
2019-02-22 01:22:24 +0300

    add all basic styling as defined in design doc

--- src/DatePicker/DatePicker.component.tsx
+++ src/DatePicker/DatePicker.component.tsx
@@ -37,17 +37,52 @@ const SOverlayComponent = styled.div`
   border: 1px solid ${(props: Props) => props.theme.colors.lightGray};
   margin-top: ${(props: Props) =>
     props.theme.common[props.datepickersize!].padding.toString().split(' ')[1]};
+  font-family: ${(props: Props) => props.theme.typography.fontFamily};
+  font-size: ${(props: Props) =>
+    props.theme.common[props.datepickersize!].fontSize}
+  color: ${(props: Props) => props.theme.colors.primary};
   box-shadow: #0000001a 0px 3px 25px;
+  .DayPicker-wrapper {
+    padding-top: 1.5em;
+    padding-bottom: 1.5em;
+  }
+  .DayPicker-NavBar {
+    position: relative;
+  }
+  .DayPicker-NavButton {
+    width: 2.25em;
+    &:focus {
+      outline: none;
+    }
+  }
   .DayPicker-NavButton--prev {
-    left: 0.8rem;
+    left: 3rem;
     background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACQAAAAkCAYAAADhAJiYAAABH0lEQVRYhe3YvY3CQBCG4Rd0BVABogUogAQhkZ+oggIoASFCApKji9OxyQVsjFgqQcQkXAAJK87enxnbAV9oaaxH6/Hu2K0bN5qUdt0AP29QWT60btwfTnrAElg5aw6hdS2Npn5g9kAXuADjUJT4I/MwAB1gEVovCnqBgfsKzSsHFWBGzppjpaB/MOdYjAioADOOxWSDpDFZIA0MJG6MJT1zSsVAwgppYqJB2pgoUBWYKBCw8TAAU0kMxIHWwNW7tuoPJx1BTzjIWfMDfHqoAfAriYpqamfNtzYq+rXXRiXt1Jqo5KNDC5V1uGqgsscPaZTIgCaJEhthpVCiQ34BalsLqAC1qw0ET6gzMHPWfIXWqny55qRxPxveoLI0DvQHF7mbWMvAdEsAAAAASUVORK5CYII=');
   }
   .DayPicker-NavButton--next {
-    right: 1rem;
+    right: 3.2rem;
     background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACQAAAAkCAYAAADhAJiYAAABNUlEQVRYhe3YPUrEQBjG8f+KbO0BRMQbpLGMbLUptvMYHmAPYLGIiJ14AcvtZxcsdkoR5gYii7VYi7AWThFikbyZ901S7NPlg/CDyTwzyWjHjiHloG9ANXtQXQ4lN2d5cQrcALfBu1cL0KjpSx0xG+AE+AKmwbsXbZBkyOYRA3AErLK8OO8TtADeS8cmqMag4N0WuLBGiWZZFyjxtLdGteohS1TrYrRCJTV1CfWmhUpeOiJqooVq3NR1yfLimL8mPyudFje62uIavPtAYfhUV/uImgI/FdRTL6AsL8bAPf93EXedgyJmCcwql66Cd4+dgmowD5JnJYM0MckgbUwSyALTGmSFaQWyxIhB1hgRqAuMCARcW2NABnoGvi0xIPvqWAOXwKcVBhT3Q1oZ3M+GPagugwP9Aj3mk1GmPwqDAAAAAElFTkSuQmCC');
   }
   .DayPicker-Caption {
     text-align: center;
+    margin-bottom: 3em;
+    margin-top: .2em;
+  }
+  .DayPicker-Caption > div, .DayPicker-Weekday {
+    color: ${(props: Props) => props.theme.colors.primary};
+    font-weight: bold;
+    font-size: ${(props: Props) =>
+      props.theme.common[props.datepickersize!].fontSize}
+  }
+  .DayPicker-Day {
+    opacity: .8;
+    padding: .7em;
+    font-size: ${(props: Props) =>
+      props.theme.common[props.datepickersize!].fontSize}
+  }
+  .DayPicker-Month {
+    margin-left: 1.5em;
+    margin-right: 1.5em;
   }
 `;
 
