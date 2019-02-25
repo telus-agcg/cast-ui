67e2c3f7a4421295d87d5d53b3e0e1d10b844368
Brian Kinyua <229225+bankai254@users.noreply.github.com>
2019-02-25 23:16:55 +0300

    Alert (#40)
    * Initial commit
    
    * Add light mode toggle and styles
    
    * Update styles
    
    * Add full width mode
    
    * Fix linting errors

---
+++ src/Alert/Alert.stories.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+import { select, boolean } from '@storybook/addon-knobs/react';
+
+import { Alert } from './Alert.component';
+import { wInfo } from '../storybook-utils';
+
+storiesOf('Alert', module).add(
+  'Alert',
+  wInfo(`
+
+  ### Notes
+
+  This is a Alert
+
+  The default theme mode is dark.
+
+  ### Usage
+  ~~~js
+  <Alert
+    alertStyle='primary'
+    lightMode=true
+    fullWidth=true
+  >
+    Reminder: Sales meeting at Rm 223 in 10 minutes
+  </Alert>
+  ~~~`)(() => (
+    <Alert
+      alertStyle={select(
+        'alertStyle',
+        ['success', 'primary', 'secondary', 'danger', 'warning'],
+        'primary',
+      )}
+      lightMode={boolean('lightMode', false)}
+      fullWidth={boolean('fullWidth', false)}
+    >
+      Reminder: Sales meeting at Rm 223 in 10 minutes
+    </Alert>
+  )),
+);
---
+++ src/Alert/index.ts
@@ -0,0 +1,2 @@
+import { Alert } from './Alert.component';
+export default Alert;
