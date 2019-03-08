38dcbb04b9db4e403e902c48d9163fea9898ba9d
Brian Kinyua <brian.kinyua@tezzasolutions.com>
2019-03-08 14:29:42 +0300

    Add updated Typography story

--- src/Typography/Typography.stories.tsx
+++ src/Typography/Typography.stories.tsx
@@ -2,20 +2,19 @@ import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import { Typography } from './Typography.component';
-import { wInfo } from '../storybook-utils';
 
 storiesOf('Typography', module).add(
   'Typography',
-  wInfo(`
-
-  ### Notes
-
-  Documentation and examples for Cast UI typography.
-
-  ### Usage
-  ~~~js
-  <Typography />
-  ~~~`)(() => (
+  () => (
     <Typography />
-  )),
+  ),
+  {
+    info: {
+      text: `
+        ### Notes
+
+        Documentation and examples for Cast UI typography.
+        `,
+    },
+  },
 );
