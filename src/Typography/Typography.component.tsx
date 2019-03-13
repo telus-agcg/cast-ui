d66287d4dd4d1b8c031867013ceea5b58807f5dd
Brian Kinyua <brian.kinyua@tezzasolutions.com>
2019-03-13 23:07:21 +0300

    Add Body style with createGlobalStyle

--- src/Typography/Typography.component.tsx
+++ src/Typography/Typography.component.tsx
@@ -1,4 +1,7 @@
 import * as React from 'react';
+import { createGlobalStyle } from 'styled-components';
+
+import 'typeface-roboto';
 
 import { Headline } from './Headline/Headline.component';
 import { SectionHeader } from './SectionHeader/SectionHeader.component';
@@ -12,11 +15,21 @@ export type Props = {
   theme?: any;
 };
 
+const GlobalDefaults = createGlobalStyle`
+  html, body{
+    font-weight: ${(props: Props) => props.theme.typography.fontWeight};
+    font-family: ${(props: Props) => props.theme.typography.fontFamily};
+    font-size: ${(props: Props) => props.theme.typography.fontSize};
+    color: ${(props: Props) => props.theme.colors.primary}
+  }
+`;
+
 export const Typography: React.FunctionComponent<Props> = ({
   children,
   theme,
 }) => (
   <div>
+    <GlobalDefaults />
     <Headline>27px Light</Headline>
     <SectionHeader>Roboto 16 Medium, line: 1 px, #8D9599</SectionHeader>
   </div>
