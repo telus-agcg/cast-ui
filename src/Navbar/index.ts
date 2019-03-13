37aa170b73a09f3feb0d9aded3e86351c1df0fe6
Dantiff <20873103+Dantiff@users.noreply.github.com>
2019-03-13 17:55:30 +0300

    correctly export the Navbar items

--- src/Navbar/index.ts
+++ src/Navbar/index.ts
@@ -1,6 +1,7 @@
 import { Navbar } from './Navbar.component';
+import { Nav } from './Nav.component';
 
 export { Navbar, Props as NavbarProps } from './Navbar.component';
 export { Nav, Props as NavProps } from './Nav.component';
 
-export default Navbar;
+export default { Navbar, Nav };
