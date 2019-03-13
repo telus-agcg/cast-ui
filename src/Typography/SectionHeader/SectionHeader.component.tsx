3cd007046d1f80e30204799bab2fda9bae200887
Brian Kinyua <brian.kinyua@tezzasolutions.com>
2019-03-13 18:15:22 +0300

    Update Section Header styling

--- src/Typography/SectionHeader/SectionHeader.component.tsx
+++ src/Typography/SectionHeader/SectionHeader.component.tsx
@@ -21,10 +21,10 @@ const SSectionHeader = styled.h1`
   font-weight: ${(props: Props) => props.theme.typography.sectionHeader.fontWeight};
   font-family: ${(props: Props) => props.theme.typography.fontFamily};
   font-size: ${(props: Props) => props.theme.typography.sectionHeader.fontSize};
-  color: ${(props: Props) => props.theme.typography.sectionHeader.color}
-  line-height: ${(props: Props) => props.theme.typography.sectionHeader.lineHeight}
-  border-bottom: 2px solid ${(props: Props) => props.theme.typography.sectionHeader.color};
-  padding-bottom: 1em;
+  color: ${(props: Props) => props.theme.colors.primary}
+  border-bottom-width: ${(props: Props) => props.theme.typography.sectionHeader.borderWidth};
+  border-bottom-color: ${(props: Props) => props.theme.typography.sectionHeader.borderColor};
+  border-bottom-style: ${(props: Props) => props.theme.typography.sectionHeader.borderStyle};
 `;
 
 export const SectionHeader: React.FunctionComponent<Props> = ({
