7c5775203b28b4016061d6fa76e5f0cbb74bff3b
Dantiff <20873103+Dantiff@users.noreply.github.com>
2019-03-13 22:14:03 +0300

    setup and export SideNavItem

--- src/Sidenav/index.ts
+++ src/Sidenav/index.ts
@@ -1,7 +1,12 @@
 import { SideNavbar } from './SideNavbar.component';
 import { SideNav } from './SideNav.component';
+import { SideNavItem } from './SideNavItem.component';
 
 export { SideNavbar, Props as SideNavbarProps } from './SideNavbar.component';
 export { SideNav, Props as SideNavProps } from './SideNav.component';
+export {
+  SideNavItem,
+  Props as SideNavItemProps,
+} from './SideNavItem.component';
 
-export default { SideNavbar, SideNav };
+export default { SideNavbar, SideNav, SideNavItem };
