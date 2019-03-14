5d25c9c835ad9bc03803e4a13d6b4d25b05f5c5c
Brian Kinyua <brian.kinyua@tezzasolutions.com>
2019-03-14 20:12:54 +0300

    Remove typeface roboto from components

--- src/Typography/Typography.component.tsx
+++ src/Typography/Typography.component.tsx
@@ -1,7 +1,4 @@
 import * as React from 'react';
-import { createGlobalStyle } from 'styled-components';
-
-import 'typeface-roboto';
 
 import { Headline } from './Headline/Headline.component';
 import { SectionHeader } from './SectionHeader/SectionHeader.component';
@@ -15,23 +12,14 @@ export type Props = {
   theme?: any;
 };
 
-const GlobalDefaults = createGlobalStyle`
-  html, body{
-    font-weight: ${(props: Props) => props.theme.typography.fontWeight};
-    font-family: ${(props: Props) => props.theme.typography.fontFamily};
-    font-size: ${(props: Props) => props.theme.typography.fontSize};
-    color: ${(props: Props) => props.theme.colors.primary}
-    line-height: ${(props: Props) => props.theme.typography.lineHeight};
-  }
-`;
-
 export const Typography: React.FunctionComponent<Props> = ({
   children,
   theme,
 }) => (
   <div>
-    <GlobalDefaults />
     <Headline>27px Light</Headline>
     <SectionHeader>Roboto 16 Medium, line: 1 px, #8D9599</SectionHeader>
+    <p>Etiam ullamcorper, metus sed luctus auctor,
+      tortor lorem auctor quam, ut condimentum massa tellus at turpis.</p>
   </div>
 );
