88f8b24d71ef577be2adf22e8b598022ab9451e7
Brian Kinyua <brian.kinyua@tezzasolutions.com>
2019-03-16 14:22:21 +0300

    Update Tooltip component with tippy/react 2.1.1

--- src/Tooltip/Tooltip.component.tsx
+++ src/Tooltip/Tooltip.component.tsx
@@ -1,15 +1,18 @@
 import * as React from 'react';
-// import styled from 'styled-components';
-// import * as ReactPopover from 'react-popover';
+import styled from 'styled-components';
 import Tippy, { TippyProps } from '@tippy.js/react';
-import 'tippy.js/dist/tippy.css';
 
-export type Props = TippyProps &
-  Partial<{
-    children: any;
-    usePointer?: boolean;
-    theme: any;
-  }>;
+export type Props = TippyProps & {
+  children: any;
+  usePointer?: boolean;
+  arrow?: true;
+  theme?: any;
+};
+
+const STippy = styled(Tippy)`
+  background: ${(props: Props) => props.theme.typography.tooltip.background};
+  border-radius: ${(props: Props) => props.theme.typography.tooltip.borderRadius};
+`;
 
 export class Tooltip extends React.Component<Props> {
   contentIsString = () => {
@@ -17,9 +20,9 @@ export class Tooltip extends React.Component<Props> {
   }
   public render() {
     return (
-      <Tippy isVisible={true} content={this.props.content} {...this.props}>
+      <STippy isVisible={true} content={this.props.content} {...this.props} arrow={true}>
         {this.props.children}
-      </Tippy>
+      </STippy>
     );
   }
 }
