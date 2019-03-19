1dcc176a4725179d4b434cf76693446b178ccd92
Dantiff <20873103+Dantiff@users.noreply.github.com>
2019-03-20 01:55:30 +0300

    Add position absolute to SOverlayComponent to ensure DatePicker appears as popover and not inline element.

--- src/DatePicker/DatePicker.component.tsx
+++ src/DatePicker/DatePicker.component.tsx
@@ -58,6 +58,7 @@ const SOverlayComponent = styled.div`
     props.theme.common[props.datepickersize!].fontSize}
   color: ${(props: Props) => props.theme.colors.primary};
   box-shadow: #0000001a 0px 3px 25px;
+  position: absolute;
   .DayPicker-wrapper {
     padding: ${(props: Props) =>
       props.theme.datepicker[props.datepickersize!].margins.dayPickerWraper}
