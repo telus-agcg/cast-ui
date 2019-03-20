8353aeee120db1a181e17aab0b689a282591e53c
Dantiff <20873103+Dantiff@users.noreply.github.com>
2019-03-20 22:43:56 +0300

    cleanup unused code

--- src/Sidenav/SideNavbar.component.tsx
+++ src/Sidenav/SideNavbar.component.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import styled from 'styled-components';
 import { SideNavContext, propsDeepSearch } from './context';
-// import { SideNavContext } from './context';
 
 export type Props = {
   /**
