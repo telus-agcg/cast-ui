745d6c411e137406e46c8e18cf410be072181071
Dantiff <20873103+Dantiff@users.noreply.github.com>
2019-03-26 18:04:15 +0300

    temporarily diable modal

--- src/Modal/Modal.stories.tsx
+++ src/Modal/Modal.stories.tsx
@@ -1,58 +1,58 @@
-import * as React from 'react';
-import { storiesOf } from '@storybook/react';
-import { boolean, select, text } from '@storybook/addon-knobs/react';
-import { action } from '@storybook/addon-actions';
+// import * as React from 'react';
+// import { storiesOf } from '@storybook/react';
+// import { boolean, select, text } from '@storybook/addon-knobs/react';
+// import { action } from '@storybook/addon-actions';
 
-import { Modal } from '../';
+// import { Modal } from '../';
 
-storiesOf('Modal', module).add(
-  'Modal',
-  () => (
-    <Modal
-      isOpen={boolean('isOpen', true)}
-      id="myModal"
-      footerContent={select(
-        'buttonType',
-        ['OkOnly', 'OkCancel', 'YesNo'],
-        'OkCancel',
-      )}
-      modalSize={select(
-        'modalSize',
-        ['sm', 'md', 'lg'],
-        'md',
-      )}
-      modalTitle={text('modalTitle', 'Hello Modal')}
-      onCancelOrNo={action('Clicked Cancel/No!')}
-      onOkOrYes={action('Clicked OK/Yes!')}
-    >
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
-    </Modal>
-  ),
-  {
-    info: {
-      text: `
-        ### Notes
+// storiesOf('Modal', module).add(
+//   'Modal',
+//   () => (
+//     <Modal
+//       isOpen={boolean('isOpen', true)}
+//       id="myModal"
+//       footerContent={select(
+//         'buttonType',
+//         ['OkOnly', 'OkCancel', 'YesNo'],
+//         'OkCancel',
+//       )}
+//       modalSize={select(
+//         'modalSize',
+//         ['sm', 'md', 'lg'],
+//         'md',
+//       )}
+//       modalTitle={text('modalTitle', 'Hello Modal')}
+//       onCancelOrNo={action('Clicked Cancel/No!')}
+//       onOkOrYes={action('Clicked OK/Yes!')}
+//     >
+//       <p>
+//         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+//         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+//         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+//         commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+//         velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+//         occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+//         mollit anim id est laborum.
+//       </p>
+//     </Modal>
+//   ),
+//   {
+//     info: {
+//       text: `
+//         ### Notes
 
-        This is a Modal, based on the react-modal component.
+//         This is a Modal, based on the react-modal component.
 
-        To open or close the modal, change the 'isOpen' prop.
+//         To open or close the modal, change the 'isOpen' prop.
 
-        To set the width of the modal, change the 'modalSize' props to either
-        - *sm* 300px
-        - *md* 500px (default)
-        - *lg* 800px
+//         To set the width of the modal, change the 'modalSize' props to either
+//         - *sm* 300px
+//         - *md* 500px (default)
+//         - *lg* 800px
 
-        In addition of the string options under *footerContent*,
-        you can pass a component instead of any of those.
-      `,
-    },
-  },
-);
+//         In addition of the string options under *footerContent*,
+//         you can pass a component instead of any of those.
+//       `,
+//     },
+//   },
+// );
