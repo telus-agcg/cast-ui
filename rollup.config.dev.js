55a01826ab1a5be6380063bd2428e586dd495cd9
Ben Dalton <benadalton@gmail.com>
2019-04-02 11:57:17 -0400

    add rollup plugin for peeDeps (#128)
    * add peerDeps plugin back
    
    * remove redundant line
    
    * add build step to CI

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -1,4 +1,5 @@
 import babel from 'rollup-plugin-babel';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import postcss from 'rollup-plugin-postcss';
@@ -16,13 +17,12 @@ const config = {
     file: pkg.main,
     format: 'es',
   },
-  external: ['React', 'ReactDOM', 'styled-components'],
   plugins: [
     postcss({ extract: false, plugins: [autoprefixer] }),
     babel({ exclude: 'node_modules/**' }),
+    peerDepsExternal(),
     localResolve(),
     resolve(),
-    commonjs(),
     filesize(),
     typescript({
       typescript: require('typescript'),
@@ -30,6 +30,7 @@ const config = {
     replace({
       'process.env.NODE_ENV': JSON.stringify('development'),
     }),
+    commonjs(),
   ],
 };
 
