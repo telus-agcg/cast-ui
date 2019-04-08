e3ee4fe6a4a3217be4dc251f5355279dd7bfb475
Brian Kinyua <brian.kinyua@technekes.com>
2019-04-08 16:59:10 +0300

    Add Props to component file, append ReactTableDefaults to component default Props, change Props type to interface and extend TableProps via Partial

--- src/Table/Table.component.tsx
+++ src/Table/Table.component.tsx
@@ -1,7 +1,7 @@
 /* tslint:disable:max-line-length */
 import * as React from 'react';
 import styled, { ThemeProvider } from 'styled-components';
-import ReactTable, { TableProps } from 'react-table';
+import ReactTable, { TableProps, ReactTableDefaults } from 'react-table';
 import { Themes } from '../themes';
 
 import { Icon } from 'react-icons-kit';
@@ -10,9 +10,121 @@ import { ic_format_align_justify as IconCondensed } from 'react-icons-kit/md/ic_
 import { ic_view_headline as IconMedium } from 'react-icons-kit/md/ic_view_headline';
 import TablePagination from '../TablePagination/TablePagination.component';
 import 'react-table/react-table.css';
-import { castTableProps } from './castTableProps';
 
-type Props = Partial<TableProps> & castTableProps;
+export interface Props extends Partial<TableProps> {
+  data: any;
+  columns: any;
+  tableSize?: string;
+  children?: any;
+  /**
+   * Specify if pagination controls should be shown
+   *
+   * @default true
+   **/
+  showPagination?: boolean;
+  /**
+   * Specify if pagination controls should be shown at the top of the table
+   *
+   * @default false
+   **/
+  showPaginationTop?: boolean;
+  /**
+   * Specify if pagination controls should be shown at the bottom of the table
+   *
+   * @default true
+   **/
+  showPaginationBottom?: boolean;
+  /**
+   * Specify if the page size options dropdown should be shown
+   *
+   * @default true
+   **/
+  showPageSizeOptions?: boolean;
+  /**
+   * Specify the available page size options
+   *
+   * @default true
+   **/
+  pageSizeOptions?: number[];
+  /**
+   * Specify the default page size
+   *
+   * @default true
+   **/
+  defaultPageSize?: number;
+  /**
+   * Specify if the current page number should be editable
+   *
+   * @default true
+   **/
+  showPageJump?: boolean;
+  /**
+   * Specify if hierarchies should be collapsed on sorting change
+   *
+   * @default true
+   **/
+  collapseOnSortingChange?: boolean;
+  /**
+   * Specify if hierarchies should be collapsed on page change
+   *
+   * @default true
+   **/
+  collapseOnPageChange?: boolean;
+  /**
+   * Specify if hierarchies should be collapsed on data change
+   *
+   * @default true
+   **/
+  collapseOnDataChange?: boolean;
+  /**
+   * Specify if scrolling should be infinite
+   *
+   * @default true
+   **/
+  freezeWhenExpanded?: boolean;
+  /**
+   * Specify if the grid should be sortable
+   *
+   * @default true
+   **/
+  sortable?: boolean;
+  /**
+   * Specify if the grid should allow multi-column sorting
+   *
+   * @default true
+   **/
+  multiSort?: boolean;
+  /**
+   * Specify if grid columns should be resizable
+   *
+   * @default true
+   **/
+  resizable?: boolean;
+  /**
+   * Specify if grid data should be filterable
+   *
+   * @default true
+   **/
+  filterable?: boolean;
+  /**
+   * Specify if grid data rows should be striped
+   *
+   * @default false
+   **/
+  striped?: boolean;
+  /**
+   * Specify if table size is adjustable
+   *
+   * @default true
+   **/
+  sizable?: boolean;
+  /**
+   * From theme provider
+   *
+   * @default defaultTheme
+   **/
+  theme?: any;
+}
 
 const SWrapperDiv = styled.div`
   background: ${(props: any) => props.theme.input.background};
@@ -108,6 +220,7 @@ const SWrapperDiv = styled.div`
 
 const initialState = { tableSize: 'md' };
 type State = Readonly<typeof initialState>;
+// const tableDefaults = ReactTableDefaults;
 
 export class Table extends React.Component<Props> {
   readonly state: State = initialState;
@@ -117,6 +230,7 @@ export class Table extends React.Component<Props> {
   }
 
   static defaultProps = {
+    ...ReactTableDefaults,
     striped: false,
     columns: [],
     tableSize: 'md',
@@ -130,6 +244,7 @@ export class Table extends React.Component<Props> {
   }
 
   render() {
+    console.log(Table.defaultProps);
     const { theme, ...props } = this.props;
     return (
       <ThemeProvider theme={(outerTheme: any) => outerTheme || theme}>
