3cba2af9da9c06965e25883a73be0f58061cf768
Brian Kinyua <brian.kinyua@technekes.com>
2019-04-08 16:56:11 +0300

    Export TableProps

--- src/Table/index.ts
+++ src/Table/index.ts
@@ -2,10 +2,10 @@ import ReactTable from 'react-table';
 import treeTableHOC from 'react-table/lib/hoc/treeTable';
 import selectTableHOC from 'react-table/lib/hoc/selectTable';
 
-import { Table } from './Table.component';
+import { Table, Props as TableProps } from './Table.component';
 
 const TreeTable = treeTableHOC(ReactTable);
 const SelectTable = selectTableHOC(ReactTable);
 const SelectTreeTable = selectTableHOC(treeTableHOC(ReactTable));
 
-export { TreeTable, SelectTable, SelectTreeTable, Table };
+export { TreeTable, SelectTable, SelectTreeTable, Table, TableProps };
