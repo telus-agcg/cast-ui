2b5048f5a8fd944a54ffcf9f29d73e8028319ab1
Dantiff <20873103+Dantiff@users.noreply.github.com>
2019-04-16 22:11:27 +0300

    use default background color for sidenav stories

--- src/Sidenav/Sidenav.stories.tsx
+++ src/Sidenav/Sidenav.stories.tsx
@@ -24,7 +24,7 @@ storiesOf('Sidenav', module).add(
     <div style={{ height: '600px', position: 'relative' }}>
       <SideNavbar
         isOpen={boolean('isOpen', true)}
-        background={text('background', '#7EDEB6')}
+        background={text('background', '')}
         height={text('height', '')}
         width={text('width', '')}
         borderLeft={text('borderLeft', '')}
