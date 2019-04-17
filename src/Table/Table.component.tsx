e4c13125cb2f0e61aef49188666917d38e0bd40e
Dorokhovich Mikhail <49234802+dorokhovich-mikhail@users.noreply.github.com>
2019-04-17 06:19:43 -0700

    using props directly (#172)
    * using props directly
    
    * wrap styled component over ReactTablee
    
    * fix collision with ReactTable classname

--- src/Table/Table.component.tsx
+++ src/Table/Table.component.tsx
@@ -30,7 +30,7 @@ export interface Props extends TableProps {
   theme?: any;
 }
 
-const SWrapperDiv = styled.div`
+const SWrapperDiv = styled(ReactTable)`
   background: ${(props: any) => props.theme.input.background};
   border: 0;
   font-family: ${(props: any) => props.theme.typography.fontFamily};
@@ -42,15 +42,15 @@ const SWrapperDiv = styled.div`
     padding-top: 20px;
   }
 
-  .ReactTable {
+  &.ReactTable {
     border: 0;
   }
 
-  .ReactTable .rt-thead.-header {
+  &.ReactTable .rt-thead.-header {
     box-shadow: none;
     color: ${(props: any) => props.theme.table.header.color};
   }
-  .ReactTable .rt-thead .rt-tr {
+  &.ReactTable .rt-thead .rt-tr {
     border-bottom-color: ${(props: any) =>
       props.theme.table.header.borderBottomColor};
     border-bottom-style: solid;
@@ -58,18 +58,18 @@ const SWrapperDiv = styled.div`
     font-weight: ${(props: any) => props.theme.table.header.fontWeight};
     text-align: left;
   }
-  .ReactTable .rt-tbody .rt-tr {
+  &.ReactTable .rt-tbody .rt-tr {
     border-bottom-color: ${(props: any) =>
       props.theme.table.row.borderBottomColor};
     border-bottom-style: solid;
     border-bottom-width: 1px;
   }
-  .ReactTable .rt-tbody .rt-tr:hover {
+  &.ReactTable .rt-tbody .rt-tr:hover {
     background-color: ${(props: any) => props.theme.table.row.hoverColor};
   }
-  .ReactTable .rt-thead .rt-th,
-  .ReactTable .rt-tbody .rt-td,
-  .ReactTable .rt-tfoot .rt-td {
+  &.ReactTable .rt-thead .rt-th,
+  &.ReactTable .rt-tbody .rt-td,
+  &.ReactTable .rt-tfoot .rt-td {
     padding: ${(props: Props) =>
       props.theme.common[props.tableSize!].tableCellPadding};
     padding-left: 0;
@@ -77,33 +77,33 @@ const SWrapperDiv = styled.div`
     border-right: 0;
   }
 
-  .ReactTable .rt-thead .rt-th.-cursor-pointer,
-  .ReactTable .rt-thead .rt-th.rt-resizable-header,
-  .ReactTable .rt-thead .rt-th.-sort-asc,
-  .ReactTable .rt-thead .rt-td.-sort-asc,
-  .ReactTable .rt-thead .rt-th.-sort-desc,
-  .ReactTable .rt-thead .rt-td.-sort-desc {
+  &.ReactTable .rt-thead .rt-th.-cursor-pointer,
+  &.ReactTable .rt-thead .rt-th.rt-resizable-header,
+  &.ReactTable .rt-thead .rt-th.-sort-asc,
+  &.ReactTable .rt-thead .rt-td.-sort-asc,
+  &.ReactTable .rt-thead .rt-th.-sort-desc,
+  &.ReactTable .rt-thead .rt-td.-sort-desc {
     box-shadow: none;
     outline: none;
   }
 
-  .ReactTable .-cursor-pointer {
+  &.ReactTable .-cursor-pointer {
     cursor: pointer;
   }
 
-  .ReactTable .rt-tbody .rt-tr-group {
+  &.ReactTable .rt-tbody .rt-tr-group {
     border: 0;
   }
 
-  .ReactTable .rt-thead .rt-th {
+  &.ReactTable .rt-thead .rt-th {
     background-repeat: no-repeat;
     background-size: 18px 18px;
     background-position: right 4px center;
   }
-  .ReactTable .rt-thead .rt-th.-sort-asc {
+  &.ReactTable .rt-thead .rt-th.-sort-asc {
     background-image: url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIzMiIgaGVpZ2h0PSIzMiIgdmlld0JveD0iMCAwIDMyIDMyIj48cGF0aCBkPSJNMTYuMDAzIDE4LjYyNmw3LjA4MS03LjA4MUwyNSAxMy40NmwtOC45OTcgOC45OTgtOS4wMDMtOSAxLjkxNy0xLjkxNnoiLz48L3N2Zz4=');
   }
-  .ReactTable .rt-thead .rt-th.-sort-desc {
+  &.ReactTable .rt-thead .rt-th.-sort-desc {
     background-image: url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIzMiIgaGVpZ2h0PSIzMiIgdmlld0JveD0iMCAwIDMyIDMyIj48cGF0aCBkPSJNMTUuOTk3IDEzLjM3NGwtNy4wODEgNy4wODFMNyAxOC41NGw4Ljk5Ny04Ljk5OCA5LjAwMyA5LTEuOTE2IDEuOTE2eiIvPjwvc3ZnPg==');
   }
 `;
@@ -125,15 +125,12 @@ export class Table extends React.Component<Props> {
     const { theme, ...props } = this.props;
     return (
       <ThemeProvider theme={(outerTheme: any) => outerTheme || theme}>
-        <SWrapperDiv {...props}>
-          <ReactTable
-            {...props}
-            className={`-highlight  + ${this.props.striped ? '-striped ' : ''}`}
-            PaginationComponent={TablePagination}
-            nextText="Next >"
-            previousText="< Previous"
-          />
-        </SWrapperDiv>
+        <SWrapperDiv
+          {...props}
+          PaginationComponent={TablePagination}
+          nextText="Next >"
+          previousText="< Previous"
+        />
       </ThemeProvider>
     );
   }
