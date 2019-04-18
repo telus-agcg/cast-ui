ca814aad312ff8d73fc1e17e4b9c3329cb2edcf8
Mikhail <mikhail.dorokhovich@technekes.com>
2019-04-18 07:02:19 -0700

    add select to the InputGroup's story

--- src/InputGroup/InputGroup.stories.tsx
+++ src/InputGroup/InputGroup.stories.tsx
@@ -2,26 +2,48 @@ import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import { InputGroup, Input } from '../';
-import { text, boolean } from '@storybook/addon-knobs';
+import Select from '../Select';
+import { text, boolean, select } from '@storybook/addon-knobs';
 
 storiesOf('InputGroup', module).add(
   'InputGroup with Title',
   () => (
-    <InputGroup
-      label={text('label', 'This is my label')}
-      vertical={boolean('vertical', false)}
-      inputSize="md"
-    >
-      <Input
-        id="myInput"
+    <div>
+      <InputGroup
+        label={text('inputLabel', 'This is my label for Input Label')}
+        vertical={boolean('inputVertical', false)}
         inputSize="md"
-        type="text"
-        disabled={false}
-        required={false}
-        autoComplete="on"
-        maxLength={1000}
-      />
-    </InputGroup>
+      >
+        <Input
+          id="myInput"
+          inputSize="md"
+          type="text"
+          disabled={false}
+          required={false}
+          autoComplete="on"
+          maxLength={1000}
+        />
+      </InputGroup>
+      <InputGroup
+        label={text('selectLabel', 'This is my label for Select Component')}
+        vertical={boolean('selectVertical', false)}
+        inputSize="md"
+      >
+        <Select
+          id="mySelect"
+          disabled={boolean('disabled', false)}
+          selectSize={select('selectSize', ['sm', 'md', 'lg'], 'md')}
+          invalid={boolean('invalid', false)}
+          invalidText={text('invalidText', 'A valid value is required')}
+          invalidTextColor={text('invalidTextColor', '')}
+          options={[
+            { value: 'chocolate', label: 'Chocolate' },
+            { value: 'strawberry', label: 'Strawberry' },
+            { value: 'vanilla', label: 'Vanilla' },
+          ]}
+        />
+      </InputGroup>
+    </div>
   ),
   {
     info: {
