7188abd8700136c9d6c8ce05e7f7988ff2885d10
Dorokhovich Mikhail <49234802+dorokhovich-mikhail@users.noreply.github.com>
2019-04-30 08:51:35 -0700

    TPMTI-118. Add few styles to the select component. (#192)
    * change styles
    
    * add default styles to the story
    
    * merge boreder-colors
    
    * implement  corret props inheritance
    
    * set default id for select
    
    * add drodown arrwo color
    
    * apply new prop selectStyle
    
    * rename indicatorColor to drodpwn Color and place it into styles
    
    * change dropdown top margin
    
    * add pointer to the select option
    
    * finished with styles for the select
    
    * add border color
    
    * copy props from SelectBase<OptionType>
    
    * remove test props
    
    * remove debug props
    
    * remove comments for prod

--- src/Select/Select.component.tsx
+++ src/Select/Select.component.tsx
@@ -1,10 +1,17 @@
 import * as React from 'react';
 import ErrorMessage from '../Typography/ErrorMessage/index';
-import Select, { components } from 'react-select';
+import Select, { components /*, SelectBase */ } from 'react-select';
+// import { SelectComponents, InputActionMeta, FocusDirection } from 'react-select/types';
 import styled, { ThemeProvider } from 'styled-components';
 import { Themes } from '../themes';
+import uuid from 'uuid';
 
-export type Props = {
+export type OptionType = {
+  value: string;
+  label: string;
+};
+
+export interface Props extends React.HTMLAttributes<HTMLDivElement> {
   /**
    * From theme provider
    *
@@ -22,7 +29,7 @@ export type Props = {
    *
    * @default null
    **/
-  id: string;
+  id?: string;
   /**
    * Specify if the tab is disabled
    *
@@ -63,46 +70,146 @@ export type Props = {
    *
    * @default null
    **/
-  controlSpecificProps?: any;
-};
+  controlSpecificProps?: {
+    blockOptionHover: boolean;
+    clearFocusValueOnUpdate: boolean;
+    commonProps: any;
+    // components: SelectComponents<OptionType>;
+    hasGroups: boolean;
+    initialTouchX: number;
+    initialTouchY: number;
+    inputIsHiddenAfterUpdate: boolean | null;
+    instancePrefix: string;
+    openAfterFocus: boolean;
+    scrollToFocusedOptionOnUpdate: boolean;
+    userIsDragging: boolean | null;
+    controlRef: React.Ref<any>;
+    getControlRef: (ref: HTMLElement) => void;
+    focusedOptionRef: React.Ref<any>;
+    getFocusedOptionRef: (ref: HTMLElement) => void;
+    menuListRef: React.Ref<any>;
+    getMenuListRef: (ref: HTMLElement) => void;
+    inputRef: React.Ref<any>;
+    getInputRef: (ref: HTMLElement) => void;
+    // cacheComponents: (components: SelectComponents<OptionType>) => void;
+    onMenuOpen(): void;
+    onMenuClose(): void;
+    // onInputChange(newValue: string, actionMeta: InputActionMeta): void;
+    focusInput(): void;
+    blurInput(): void;
+    focus(): void;
+    blur(): void;
+    openMenu(focusOption: 'first' | 'last'): void;
+    focusValue(direction: 'previous' | 'next'): void;
+    // focusOption(direction: FocusDirection): void;
+    // setValue: (
+    //   newValue: ValueType<OptionType>,
+    //   action: ActionTypes,
+    //   option?: OptionType
+    // ) => void;
+    selectOption: (newValue: OptionType) => void;
+    removeValue: (removedValue: OptionType) => void;
+    clearValue: () => void;
+    popValue: () => void;
+    children: React.ReactElement;
+  };
+  /**
+   * Color of the input's border
+   *
+   * @default: ''
+   */
+  borderColor?: string;
+  /**
+   * Color of the input's radius
+   *
+   * @default: ''
+   */
+  borderRadius?: string;
+  /**
+   * color of the arrow down
+   *
+   * @default ''
+   */
+  dropdownColor?: string;
+  /**
+   * option color representation
+   *
+   * @default '''
+   */
+  optionBackgroundColor?: string;
+  /**
+   * option's hover color representation
+   *
+   * @default '''
+   */
+  hoverOptionBackgroundColor?: string;
+}
 
-const SDiv = styled.div`
+const SDiv = styled.div<Props>`
   font-family: ${(props: Props) => props.theme.typography.fontFamily};
   font-size: ${(props: Props) => props.theme.common[props.selectSize!].fontSize}
   color: ${(props: Props) => props.theme.reverseText};
   .react-select-component > div {
     min-height: unset;
-    border-color: ${(props: any) =>
-      props.invalid && props.theme.validation.borderColor};
+    margin-top: 1px;
+    padding-top: 1px;
+    border-radius: ${(props: Props) =>
+      props.borderRadius || props.theme.common[props.selectSize!].borderRadius};
+    border-color: ${(props: Props) =>
+      props.borderColor || props.theme.select.borderColor || 'inherit'};
   }
 `;
 
 const SSelect = styled(Select)`
-  border-radius: ${(props: Props) =>
-    props.theme.common[props.selectSize!].borderRadius};
   &:disabled {
     background: ${(props: Props) => props.theme.input.backgroundDisabled};
     cursor: not-allowed;
   }
 `;
 
+const SIndicatorWrapper = styled.div<Props>`
+  & > div > div {
+    color: ${(props: any) =>
+      props.dropdownColor || props.theme.select.dropdownColor};
+    &:hover {
+      color: ${(props: Props) =>
+        props.dropdownColor || props.theme.select.dropdownColor};
+    }
+  }
+  & > div > span {
+    color: ${(props: Props) =>
+      props.dropdownColor || props.theme.select.dropdownColor};
+  }
+`;
+
+const SOption = styled.div<Props>`
+  backgroundcolor: ${(props: Props) =>
+    props.optionBackgroundColor || props.theme.select.optionBackgroundColor};
+  &:hover {
+    backgroundcolor: ${(props: Props) =>
+      props.hoverOptionBackgroundColor ||
+      props.theme.select.hoverOptionBackgroundColor};
+  }
+`;
+
+const Option = styled(components.Option)``;
+
 const IndicatorsContainer = styled(components.IndicatorsContainer)`
   & > div {
     padding-top: 0;
     padding-bottom: 0;
-  }
 `;
 
 export class CustomSelect extends React.Component<Props> {
-  constructor(props: Props) {
-    super(props);
-  }
-
   static defaultProps = {
     selectSize: 'md',
     theme: Themes.defaultTheme,
     invalidText: '',
     invalidTextColor: '',
+    id: uuid.v4(),
+    option: {},
+    optionBackgroundColor: '',
+    hoverOptionBackgroundColor: '',
   };
 
   render() {
@@ -118,6 +225,10 @@ export class CustomSelect extends React.Component<Props> {
       selectedOption,
       invalidText,
       invalidTextColor,
+
+      dropdownColor,
+      optionBackgroundColor,
+      hoverOptionBackgroundColor,
       ...props
     } = this.props;
     const errorId = invalid ? `${id}-error-msg` : '';
@@ -134,7 +245,21 @@ export class CustomSelect extends React.Component<Props> {
           {...props}
         >
           <SSelect
-            components={{ IndicatorsContainer }}
+            components={{
+              Option: p => (
+                <SOption
+                  optionBackgroundColor={optionBackgroundColor}
+                  hoverOptionBackgroundColor={hoverOptionBackgroundColor}
+                >
+                  <Option {...p} />
+                </SOption>
+              ),
+              IndicatorsContainer: p => (
+                <SIndicatorWrapper dropdownColor={dropdownColor}>
+                  <IndicatorsContainer {...p} />
+                </SIndicatorWrapper>
+              ),
+            }}
             closeMenuOnSelect={false}
             className="react-select-component"
             isDisabled={disabled}
@@ -144,6 +269,7 @@ export class CustomSelect extends React.Component<Props> {
             aria-invalid={invalid ? true : undefined}
             aria-describedby={errorId}
             selectSize={selectSize}
+            dropdownColor={dropdownColor}
             {...props}
             {...controlSpecificProps}
           />
