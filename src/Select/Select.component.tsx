8218fff573ccc76e05a5588e97c2e74b8c93fdde
Mikhail <mikhail.dorokhovich@technekes.com>
2019-04-30 08:28:39 -0700

    copy props from SelectBase<OptionType>

--- src/Select/Select.component.tsx
+++ src/Select/Select.component.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import ErrorMessage from '../Typography/ErrorMessage/index';
-import Select, { components, SelectBase } from 'react-select';
+import Select, { components /*, SelectBase */ } from 'react-select';
 // import { SelectComponents, InputActionMeta, FocusDirection } from 'react-select/types';
 import styled, { ThemeProvider } from 'styled-components';
 import { Themes } from '../themes';
@@ -26,49 +26,49 @@ interface AnotherProps {
   bar?: string;
 }
 
-export interface ControlSpecificProps extends SelectBase<OptionType> {
-  // blockOptionHover: boolean;
-  // clearFocusValueOnUpdate: boolean;
-  // commonProps: any;
-  // // components: SelectComponents<OptionType>;
-  // hasGroups: boolean;
-  // initialTouchX: number;
-  // initialTouchY: number;
-  // inputIsHiddenAfterUpdate: boolean | null;
-  // instancePrefix: string;
-  // openAfterFocus: boolean;
-  // scrollToFocusedOptionOnUpdate: boolean;
-  // userIsDragging: boolean | null;
-  // controlRef: React.Ref<any>;
-  // getControlRef: (ref: HTMLElement) => void;
-  // focusedOptionRef: React.Ref<any>;
-  // getFocusedOptionRef: (ref: HTMLElement) => void;
-  // menuListRef: React.Ref<any>;
-  // getMenuListRef: (ref: HTMLElement) => void;
-  // inputRef: React.Ref<any>;
-  // getInputRef: (ref: HTMLElement) => void;
-  // // cacheComponents: (components: SelectComponents<OptionType>) => void;
-  // onMenuOpen(): void;
-  // onMenuClose(): void;
-  // // onInputChange(newValue: string, actionMeta: InputActionMeta): void;
-  // focusInput(): void;
-  // blurInput(): void;
-  // focus(): void;
-  // blur(): void;
-  // openMenu(focusOption: 'first' | 'last'): void;
-  // focusValue(direction: 'previous' | 'next'): void;
-  // // focusOption(direction: FocusDirection): void;
-  // // setValue: (
-  // //   newValue: ValueType<OptionType>,
-  // //   action: ActionTypes,
-  // //   option?: OptionType
-  // // ) => void;
-  // selectOption: (newValue: OptionType) => void;
-  // removeValue: (removedValue: OptionType) => void;
-  // clearValue: () => void;
-  // popValue: () => void;
-  children: React.ReactElement;
-}
+// export interface ControlSpecificProps extends SelectBase<OptionType> {
+//   blockOptionHover: boolean;
+//   clearFocusValueOnUpdate: boolean;
+//   commonProps: any;
+//   // components: SelectComponents<OptionType>;
+//   hasGroups: boolean;
+//   initialTouchX: number;
+//   initialTouchY: number;
+//   inputIsHiddenAfterUpdate: boolean | null;
+//   instancePrefix: string;
+//   openAfterFocus: boolean;
+//   scrollToFocusedOptionOnUpdate: boolean;
+//   userIsDragging: boolean | null;
+//   controlRef: React.Ref<any>;
+//   getControlRef: (ref: HTMLElement) => void;
+//   focusedOptionRef: React.Ref<any>;
+//   getFocusedOptionRef: (ref: HTMLElement) => void;
+//   menuListRef: React.Ref<any>;
+//   getMenuListRef: (ref: HTMLElement) => void;
+//   inputRef: React.Ref<any>;
+//   getInputRef: (ref: HTMLElement) => void;
+//   // cacheComponents: (components: SelectComponents<OptionType>) => void;
+//   onMenuOpen(): void;
+//   onMenuClose(): void;
+//   // onInputChange(newValue: string, actionMeta: InputActionMeta): void;
+//   focusInput(): void;
+//   blurInput(): void;
+//   focus(): void;
+//   blur(): void;
+//   openMenu(focusOption: 'first' | 'last'): void;
+//   focusValue(direction: 'previous' | 'next'): void;
+//   // focusOption(direction: FocusDirection): void;
+//   // setValue: (
+//   //   newValue: ValueType<OptionType>,
+//   //   action: ActionTypes,
+//   //   option?: OptionType
+//   // ) => void;
+//   selectOption: (newValue: OptionType) => void;
+//   removeValue: (removedValue: OptionType) => void;
+//   clearValue: () => void;
+//   popValue: () => void;
+//   children: React.ReactElement;
+// }
 
 export interface Props
   extends React.HTMLAttributes<HTMLDivElement>,
@@ -131,7 +131,49 @@ export interface Props
    *
    * @default null
    **/
-  controlSpecificProps?: ControlSpecificProps;
+  controlSpecificProps?: {
+    blockOptionHover: boolean;
+    clearFocusValueOnUpdate: boolean;
+    commonProps: any;
+    // components: SelectComponents<OptionType>;
+    hasGroups: boolean;
+    initialTouchX: number;
+    initialTouchY: number;
+    inputIsHiddenAfterUpdate: boolean | null;
+    instancePrefix: string;
+    openAfterFocus: boolean;
+    scrollToFocusedOptionOnUpdate: boolean;
+    userIsDragging: boolean | null;
+    controlRef: React.Ref<any>;
+    getControlRef: (ref: HTMLElement) => void;
+    focusedOptionRef: React.Ref<any>;
+    getFocusedOptionRef: (ref: HTMLElement) => void;
+    menuListRef: React.Ref<any>;
+    getMenuListRef: (ref: HTMLElement) => void;
+    inputRef: React.Ref<any>;
+    getInputRef: (ref: HTMLElement) => void;
+    // cacheComponents: (components: SelectComponents<OptionType>) => void;
+    onMenuOpen(): void;
+    onMenuClose(): void;
+    // onInputChange(newValue: string, actionMeta: InputActionMeta): void;
+    focusInput(): void;
+    blurInput(): void;
+    focus(): void;
+    blur(): void;
+    openMenu(focusOption: 'first' | 'last'): void;
+    focusValue(direction: 'previous' | 'next'): void;
+    // focusOption(direction: FocusDirection): void;
+    // setValue: (
+    //   newValue: ValueType<OptionType>,
+    //   action: ActionTypes,
+    //   option?: OptionType
+    // ) => void;
+    selectOption: (newValue: OptionType) => void;
+    removeValue: (removedValue: OptionType) => void;
+    clearValue: () => void;
+    popValue: () => void;
+    children: React.ReactElement;
+  };
   /**
    * Color of the input's border
    *
