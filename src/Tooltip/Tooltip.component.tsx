2772ac68d79e7b3cc20ca522e6cf690584292669
Mikhail <mikhail.dorokhovich@technekes.com>
2019-05-13 05:22:22 -0700

    remove comments and fix arrow color

--- src/Tooltip/Tooltip.component.tsx
+++ src/Tooltip/Tooltip.component.tsx
@@ -11,18 +11,16 @@ const STippy = styled(Tippy)`
   background: ${(props: Props) => props.theme.tooltip.background};
   border-radius: ${(props: Props) => props.theme.tooltip.borderRadius};
   font-family: ${(props: Props) => props.theme.typography.fontFamily};
+  &[x-placement^='top'] .tippy-arrow {
+    border-color: ${(props: Props) => props.theme.tooltip.background}
+      transparent;
+  }
 `;
 
 export class Tooltip extends React.Component<Props> {
   static defaultProps = {
     arrow: true,
     theme: Themes.defaultTheme,
-  };
-  /**
-   * What the reason of this function?
-   */
-  contentIsString = () => {
-    return typeof this.props.content === 'string';
   };
   public render() {
     const { theme, children, ...props } = this.props;
