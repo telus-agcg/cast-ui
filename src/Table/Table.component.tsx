0f906f199dc723583d4ef5d56859002361e23439
andrey.liventsev <andrey.liventsev@ontarget-group.com>
2019-07-01 11:02:24 +0300

    CCIBI-354 Some Product Overview table values are not aligned at right Fix right-alignment for flex elements

--- src/Table/Table.component.tsx
+++ src/Table/Table.component.tsx
@@ -51,7 +51,8 @@ const SWrapperDiv = styled(ReactTable)`
   width: 100%;
   box-sizing: border-box;
   .right-align {
-    text-align: right;
+    text-align: right; // for blocked elements
+    justify-content: flex-end; // for flex elements
   }
 
   .right-align.-sort-asc,
