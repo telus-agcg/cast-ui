1f0efccaca005510a3defd43a8384e8f5e8be222
andrey.liventsev <andrey.liventsev@ontarget-group.com>
2019-09-29 22:21:26 +0300

    Collapsible Panel: add iconPosition parameter, edit styles

--- src/themes/cobalt.ts
+++ src/themes/cobalt.ts
@@ -527,7 +527,7 @@ export const cobaltTheme = {
   },
   panel: {
     header: {
-      padding: '14px 16px 14px 30px',
+      padding: '14px 16px',
     },
     body: {
       padding: '30px 30px',
