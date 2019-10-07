165ee84b8cea3eefa263529cc3704c0614452353
Mikhail <mikhail.dorokhovich@technekes.com>
2019-10-07 04:54:32 -0700

    added story for toast

--- src/Toast/Toast.component.tsx
+++ src/Toast/Toast.component.tsx
@@ -1,7 +1,71 @@
 import * as React from 'react';
+import Alert, { AlertProps } from '../Alert';
+import styled, { ThemeProvider } from 'styled-components';
 
-export type Props = {};
+export enum PositionEnum {
+  LEFT = 'left',
+  RIGHT = 'right',
+  TOP = 'top',
+  BOTTOM = 'bottom',
+}
 
-export const Toast = (props: Props) => {
-  return null;
+export enum PositionType {
+  ABSOLUTE = 'absolute',
+  FIXED = 'fixed',
+}
+
+export interface Props extends React.HTMLAttributes<HTMLDivElement> {
+  alertProps?: AlertProps;
+  position?: string;
+  children: any;
+  /**
+   * From theme provider
+   *
+   * @default defaultTheme
+   **/
+  theme?: any;
+}
+
+const calcPosition = (position: string): string[] => position.split(' ');
+
+const hasPosition = (position: string, props: Partial<Props>) =>
+  calcPosition(props.position!).includes(position);
+
+const ToastWrapper = styled.div<Partial<Props>>`
+  position: fixed;
+  top: ${props =>
+    hasPosition(PositionEnum.TOP, props)
+      ? /* props.theme.toast.position.top */ '20px'
+      : 'unset'};
+  left: ${props =>
+    hasPosition(PositionEnum.LEFT, props)
+      ? /* props.theme.toast.position.left */ '20px'
+      : 'unset'};
+  right: ${props =>
+    hasPosition(PositionEnum.RIGHT, props)
+      ? /* props.theme.toast.position.right */ '20px'
+      : 'unset'};
+  bottom: ${props =>
+    hasPosition(PositionEnum.BOTTOM, props)
+      ? /* props.theme.toast.position.bottom */ '20px'
+      : 'unset'};
+  position: ${props =>
+    (hasPosition(PositionType.ABSOLUTE, props) && PositionType.ABSOLUTE) ||
+    (hasPosition(PositionType.FIXED, props) && PositionType.FIXED) ||
+    PositionType.FIXED};
+`;
+
+export const Toast = ({ alertProps, theme, children, ...props }: Props) => {
+  console.log(props.position);
+  return (
+    <ThemeProvider theme={(outerTheme: any) => outerTheme || theme}>
+      <ToastWrapper {...props}>
+        <Alert {...alertProps}>{children}</Alert>
+      </ToastWrapper>
+    </ThemeProvider>
+  );
+};
+
+Toast.defaultProps = {
+  position: 'top right',
 };
--- src/Toast/Toast.stories.tsx
+++ src/Toast/Toast.stories.tsx
@@ -1,5 +1,47 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
-import { Toast } from '../';
+import { select } from '@storybook/addon-knobs/react';
+import { Toast } from '../Toast';
 
-storiesOf('Tabnav', module).add('Toast', () => <div />);
+storiesOf('Toast', module)
+  .add('Custom Toast', () => (
+    <div
+      style={{
+        position: 'relative',
+        width: '100%',
+        height: '100px',
+        border: '1px solid rgba(0,0,0,0.1)',
+      }}
+    >
+      <Toast
+        position={select(
+          'position',
+          [
+            'fixed top left',
+            'fixed top right',
+            'fixed bottom left',
+            'fixed bottom right',
+            'absolute top left',
+            'absolute top right',
+            'absolute bottom left',
+            'absolute bottom right',
+          ],
+          'fixed top left',
+        )}
+      >
+        TOASTER
+      </Toast>
+    </div>
+  ))
+  .add('Default Toast', () => (
+    <div
+      style={{
+        position: 'relative',
+        width: '100%',
+        height: '100px',
+        border: '1px solid rgba(0,0,0,0.1)',
+      }}
+    >
+      <Toast>TOASTER</Toast>
+    </div>
+  ));
