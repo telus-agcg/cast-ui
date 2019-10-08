cc97ff95ffd905ccdd61f429fc33f9ed5f07084e
Mikhail <mikhail.dorokhovich@technekes.com>
2019-10-08 06:59:19 -0700

    added common slider component

---
+++ src/Slider/Slider.component.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export interface Props {}
+
+export const Slider = (props: Props) => {
+  return <div>Slider</div>;
+};
+
+export default Slider;
---
+++ src/Slider/Slider.stories.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import { Slider } from './';
+
+storiesOf('Slider', module).add('Default Slider', () => <Slider />);
