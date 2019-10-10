9ac75eb91c9e4e085356f7503284e47261b3e623
Your Name <mikhail.dorokhovich@technekes.com>
2019-10-10 06:41:17 -0700

    added slider component

--- src/Slider/Slider.component.tsx
+++ src/Slider/Slider.component.tsx
@@ -1,9 +1,46 @@
 import * as React from 'react';
+import styled, { ThemeProvider } from 'styled-components';
+import InputRange, { InputRangeProps } from 'react-input-range'
+import 'react-input-range/lib/css/index.css'
 
-export interface Props {}
+export interface Props extends InputRangeProps {
+  /**
+   * From theme provider
+   *
+   * @default defaultTheme
+   **/
+  theme?: any;
+}
 
-export const Slider = (props: Props) => {
-  return <div>Slider</div>;
-};
+const SInputRangeWrapper = styled.div`
+  margin-bottom: 40px;
+  .input-range {
+    height: 18px;
+  }
+  .input-range__slider {
+    background-color: ${({ theme }) => theme.colors.primary};
+    border-color: ${({ theme }) => theme.colors.primary};
+    width: 12px;
+    height: 12px;
+  }
+  .input-range__track--active {
+    background-color: ${({ theme }) => theme.colors.primary};
+  }
+  .input-range__label--min .input-range__label-container,
+  .input-range__label--max .input-range__label-container {
+    display: none;
+  }
+  .input-range__label-container {
+    color: ${({ theme }) => theme.typography.color};
+  }
+`
+
+export const Slider = ({theme, ...props}: Props) => 
+  <ThemeProvider theme={(outerTheme: any) => outerTheme || theme}>
+    <SInputRangeWrapper>
+      <InputRange {...props}/>
+    </SInputRangeWrapper>
+  </ThemeProvider>
+;
 
 export default Slider;
