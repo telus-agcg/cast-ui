7e67d32cdabe8302e3666ab0168772483a736d8a
Dorokhovich Mikhail <49234802+dorokhovich-mikhail@users.noreply.github.com>
2019-11-13 13:30:09 -0800

    CCIBI-1377 Add Toast component (#486)
    * added toast folder
    
    * common fill for toast
    
    * added story for toast
    
    * added required props
    
    * added animation for toast
    
    * added toast stack
    
    * added styles for toast
    
    * finished styling toast and created simple toaststack
    
    * removed useless handler
    
    * added closable dismissable and missed documentation
    
    * added toast position to all themes
    
    * update toast
    
    * update version

---
+++ src/Toast/Toast.stories.tsx
@@ -0,0 +1,61 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+import { select, boolean } from '@storybook/addon-knobs/react';
+import { Toast } from '../Toast';
+import { ToastStyleEnum } from './Toast.component';
+
+storiesOf('Toast', module)
+  .add('Custom Toast', () => (
+    <div
+      style={{
+        position: 'relative',
+        width: '100%',
+        height: '100px',
+        border: '1px solid rgba(0,0,0,0.1)',
+      }}
+    >
+      <Toast
+        position={select(
+          'position',
+          [
+            'fixed top left',
+            'fixed top right',
+            'fixed bottom left',
+            'fixed bottom right',
+            'absolute top left',
+            'absolute top right',
+            'absolute bottom left',
+            'absolute bottom right',
+          ],
+          'fixed top left',
+        )}
+        active={boolean('active', true)}
+        lightmode={boolean('lightmode', true)}
+        toastStyle={select(
+          'toastStyle',
+          [
+            ToastStyleEnum.PRIMARY,
+            ToastStyleEnum.SECONDARY,
+            ToastStyleEnum.SUCCESS,
+            ToastStyleEnum.WARNING,
+            ToastStyleEnum.DANGER,
+          ],
+          ToastStyleEnum.PRIMARY,
+        )}
+      >
+        TOASTER
+      </Toast>
+    </div>
+  ))
+  .add('Default Toast', () => (
+    <div
+      style={{
+        position: 'relative',
+        width: '100%',
+        height: '100px',
+        border: '1px solid rgba(0,0,0,0.1)',
+      }}
+    >
+      <Toast>TOASTER</Toast>
+    </div>
+  ));
