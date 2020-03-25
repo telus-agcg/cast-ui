6fd075e6048998c9bb0818081f524e098679035c
Dorokhovich Mikhail <mikhail.dorokhovich@technekes.com>
2020-03-25 05:04:31 -0700

    reversed isOpen flag behavior. Changed logk for height calculation

--- src/ListGroup/ListGroup.component.tsx
+++ src/ListGroup/ListGroup.component.tsx
@@ -212,7 +212,7 @@ export class ListGroup extends React.Component<Props /* , State */> {
                   ...props,
                 })}
               </SListHeader>
-              <Collapse isOpen={isCollapsed}>{children}</Collapse>
+              <Collapse isOpen={!isCollapsed}>{children}</Collapse>
             </React.Fragment>
           ) : (
             [children]
