0c76afe7cba591221097ca5a9f47053f67f4220b
Tamer Dakhlallah <tdakhla@gmail.com>
2022-04-19 13:13:10 -0400

    fix(input): update input padding and spacing

--- src/themes/default.ts
+++ src/themes/default.ts
@@ -383,17 +383,17 @@ export const defaultTheme = {
     },
     sm: {
       height: '26px',
-      padding: '6px 7px',
+      padding: '4px 7px',
       iconTop: '6px',
     },
     md: {
       height: '30px',
-      padding: '9px 10px 7px 10px',
+      padding: '8px 10px',
       iconTop: '7px',
     },
     lg: {
       height: '38px',
-      padding: '11px 13px 13px 13px',
+      padding: '11px 16px 13px',
       iconTop: '9px',
     },
   },
