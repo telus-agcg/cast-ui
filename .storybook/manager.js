32701bd2bd496c6098d37de6fe4e0d3bbc549590
pnzau <19998833+pnzau@users.noreply.github.com>
2022-04-29 17:51:59 +0300

    Refactor storybook config files

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,15 +1,15 @@
 import { create } from '@storybook/theming';
 import { addons } from '@storybook/addons';
 
+const theme = create({
+  base: 'light',
+  brandTitle: 'Cast UI',
+  brandUrl: 'https://github.com/technekes/cast-ui',
+  brandImage: 'https://cdn2.hubspot.net/hubfs/1976913/TKXS-brand/tkxs-logo.png',
+});
+
 addons.setConfig({
-  theme: create({
-    base: 'light',
-    brandTitle: 'Cast UI',
-    brandUrl: 'https://github.com/technekes/cast-ui',
-    // To control appearance:
-    brandImage:
-      'https://cdn2.hubspot.net/hubfs/1976913/TKXS-brand/tkxs-logo.png',
-  }),
+  theme,
   isFullscreen: false,
   showPanel: true,
   panelPosition: 'bottom',
