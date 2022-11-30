70093d22ac02e6ec7f6738e573ecb60e94974648
Tamer Dakhlallah <tdakhla@gmail.com>
2022-11-30 16:50:56 -0500

    Updates

--- src/Sidenav/CollapsedSubMenu.component.tsx
+++ src/Sidenav/CollapsedSubMenu.component.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import Link from '../Typography/Link';
 import styled from 'styled-components';
 
 const SidebarLink = styled(Link)`
