dc21c70a50dd572392962782b4d408409c183d24
Tamer Dakhlallah <tdakhla@gmail.com>
2023-05-26 10:28:52 -0400

    Update color

--- src/themes/cobalt.ts
+++ src/themes/cobalt.ts
@@ -406,11 +406,7 @@ export const cobaltTheme = {
     borderWidth: '1px',
     borderStyle: 'solid',
     borderColor: cobaltColors.primary,
-<<<<<<< HEAD
-    disabledCheck: cobaltColors.lt800,
-=======
     disabledCheck: cobaltColors.drk400,
->>>>>>> d2fbedb (fix(colors): update radio & checkbox disabled text)
     disabledText: cobaltColors.drk400,
     unselectedColor: cobaltColors.white,
     selectedColor: cobaltColors.primary,
