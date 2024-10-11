0c59e67418d52fb35c06536d2a7cf50f5aac1233
Austine Mwanyalo <mwanyaloa@outlook.com>
2024-10-11 16:10:40 +0300

    feat(select): Add ellipsis and items counter in multi select (#1120)
    * feat(select): Add ellipsis and items counter in multi select
    
    * code cleanup to fix build errors

--- src/Select/SelectCheckbox.component.tsx
+++ src/Select/SelectCheckbox.component.tsx
@@ -1,18 +1,8 @@
-import * as React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 import Checkbox from '../Checkbox';
 import _ from 'lodash';
-
-const isObject = val => {
-  if (val === null) {
-    return false;
-  }
-  return typeof val === 'object';
-};
-
-const isEmptyObject = val => {
-  return isObject(val) && Object.keys(val).length === 0;
-};
+import { components, ValueContainerProps } from 'react-select';
 
 interface Props {
   options: any[];
@@ -23,29 +13,177 @@ interface Props {
   clearText?: any;
 }
 
+interface TruncatedValuesProps {
+  selectedOptions: any;
+  maxWidth: number;
+}
+
 const SCheckbox = styled(Checkbox)`
   padding-bottom: 0px;
 `;
 
-const Div = styled.div`
-  margin-right: 5px;
+const SFlex = styled.div`
+  display: flex;
+  align-items: center;
+  white-space: nowrap;
+  overflow: hidden;
+  width: 100%;
 `;
 
-const MenuItemWrapper = styled.div`
-  max-height: 300px;
-  overflow-y: auto;
-`;
+const TruncatedValues = ({
+  selectedOptions,
+  maxWidth,
+}: TruncatedValuesProps) => {
+  const [displayedValue, setDisplayedValue] = useState('');
 
-export const SelectCheckboxProps = (res: Props) => {
-  const {
-    selectedOptions,
-    options,
-    updateSelectedOptions,
-    isMulti,
-    id,
-    clearText,
-  } = res;
+  useEffect(() => {
+    if (selectedOptions) {
+      let itemsToShow: string[] = [];
+      let restCount = 0;
 
+      const measureWidth = () => {
+        requestAnimationFrame(() => {
+          itemsToShow = [];
+          let currentTextWidth = 0;
+
+          // Create a temporary element to measure the width of the ellipsis text
+          const createTempElement = text => {
+            const tempSpan = document.createElement('span');
+            tempSpan.style.visibility = 'hidden';
+            tempSpan.style.position = 'absolute';
+            tempSpan.innerText = text;
+            document.body.appendChild(tempSpan);
+            return tempSpan;
+          };
+
+          // Measure the width of the ellipsis with remaining items count
+          const ellipsisElement = createTempElement(
+            `... (+${selectedOptions.length})..`,
+          );
+
+          const ellipsisWidth = ellipsisElement.scrollWidth;
+          document.body.removeChild(ellipsisElement);
+
+          for (let i = 0; i < selectedOptions.length; i += 1) {
+            const label = selectedOptions[i].label;
+            const currentText = [...itemsToShow, label].join(', ');
+
+            const tempTextElement = createTempElement(currentText);
+            currentTextWidth = tempTextElement.scrollWidth;
+
+            if (currentTextWidth + ellipsisWidth < maxWidth) {
+              itemsToShow.push(label);
+            } else {
+              restCount = selectedOptions.length - i;
+
+              // Truncate the current label to fit remaining space with ellipsis
+              const remainingWidth =
+                maxWidth -
+                (currentTextWidth - tempTextElement.scrollWidth) -
+                ellipsisWidth;
+              let truncatedLabel = label;
+
+              while (remainingWidth > 0 && truncatedLabel.length > 0) {
+                const tempTruncateElement = createTempElement(truncatedLabel);
+                truncatedLabel = truncatedLabel.slice(0, -1);
+                tempTruncateElement.innerText = [
+                  ...itemsToShow,
+                  truncatedLabel,
+                ].join(', ');
+                document.body.appendChild(tempTruncateElement);
+
+                const truncateWidth = tempTruncateElement.scrollWidth;
+                document.body.removeChild(tempTruncateElement);
+                if (
+                  truncateWidth + ellipsisWidth < maxWidth &&
+                  truncatedLabel
+                ) {
+                  const item =
+                    selectedOptions.length === 1
+                      ? `${truncatedLabel}...`
+                      : truncatedLabel;
+                  itemsToShow.push(item);
+                  restCount = selectedOptions.length === 1 ? 0 : restCount;
+                  break;
+                }
+              }
+              document.body.removeChild(tempTextElement);
+
+              break;
+            }
+          }
+          const displayedOptions = itemsToShow.join(', ');
+          const remainingText = restCount > 0 ? `... (+${restCount}) ` : '';
+
+          const newDisplayedText = `${displayedOptions}${remainingText}`;
+          if (newDisplayedText !== displayedValue) {
+            setDisplayedValue(newDisplayedText);
+          }
+        });
+      };
+
+      measureWidth();
+    }
+  }, [selectedOptions, maxWidth, displayedValue]);
+
+  return <div>{displayedValue}</div>;
+};
+
+const ValueContainer = ({
+  children,
+  ...props
+}: ValueContainerProps<any, true>) => {
+  const [values, input] = children as [any[], JSX.Element];
+  const [maxWidth, setMaxWidth] = useState(0);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const hasSelectedOptions = props.hasValue && values.length > 0;
+
+  useEffect(() => {
+    const resizeObserver = new ResizeObserver(() => {
+      if (containerRef.current) {
+        setMaxWidth(containerRef.current.offsetWidth);
+      }
+    });
+
+    if (containerRef.current) {
+      resizeObserver.observe(containerRef.current);
+    }
+
+    return () => {
+      if (containerRef.current) {
+        resizeObserver.unobserve(containerRef.current);
+      }
+    };
+  }, [containerRef]);
+
+  return (
+    <components.ValueContainer {...props}>
+      <SFlex ref={containerRef}>
+        {!hasSelectedOptions ? (
+          <span className="react-select__placeholder">
+            {props.selectProps.placeholder}
+          </span>
+        ) : (
+          <TruncatedValues
+            selectedOptions={props.getValue()}
+            maxWidth={maxWidth}
+          />
+        )}
+
+        {input}
+      </SFlex>
+    </components.ValueContainer>
+  );
+};
+
+export const SelectCheckboxProps = ({
+  selectedOptions,
+  options,
+  updateSelectedOptions,
+  isMulti,
+  id,
+  clearText,
+}: Props) => {
   const selectMulti = (val, updateSelectedOptions) => {
     const isSelectedOption = selectedOptions.find(o => o.value === val);
     let res: any[] = [];
@@ -71,6 +209,7 @@ export const SelectCheckboxProps = (res: Props) => {
   };
 
   const components = {
+    ValueContainer,
     Option: (props: any) => {
       return (
         <div
@@ -93,39 +232,6 @@ export const SelectCheckboxProps = (res: Props) => {
         </div>
       );
     },
-    MultiValue: (props: any) => {
-      const withComma = `${props.data.label}, `;
-
-      const showCommas =
-        isMulti &&
-        selectedOptions &&
-        selectedOptions[selectedOptions.length - 1].value !== props.data.value;
-      return (
-        <div>
-          {showCommas ? (
-            <Div>{withComma} &#32;</Div>
-          ) : (
-            <Div>{props.data.label}</Div>
-          )}
-        </div>
-      );
-    },
-    MenuList: (props: any) => {
-      const selectedCount = Array.isArray(selectedOptions)
-        ? selectedOptions.length
-        : !isEmptyObject(selectedOptions)
-        ? 1
-        : 0;
-      return (
-        <MenuItemWrapper>
-          <div className={'menuListHeader'}>{`${selectedCount} item${
-            selectedCount === 1 ? '' : 's'
-          } selected`}</div>
-          {props.children}
-        </MenuItemWrapper>
-      );
-    },
-
     ClearIndicator: props => {
       const {
         innerProps: { ref, ...restInnerProps },
