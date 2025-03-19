8180d4ca86c2b10011a4d7cea9da9a00ad0d3892
Seth Holloway <seth.holloway@tkxs.com>
2025-03-19 09:38:13 -0400

    feat(Tabs): Fixing Tab styles by reverting to Class component

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,10 @@ export { Collapse } from './Collapse/Collapse.component';
 export { Input } from './Input/Input.component';
 export { Menu } from './Menu/Menu.component.tsx';
 export { CustomSelect as Select } from './Select/Select.component.tsx';
+export { Tab } from './Tabs/Tab.component.tsx';
+export { TabList } from './Tabs/TabList.component.tsx';
+export { TabPanel } from './Tabs/TabPanel.component.tsx';
+export { Tabs } from './Tabs/Tabs.component.tsx';
 export * from './themes';
 export * from './Typography';
 
