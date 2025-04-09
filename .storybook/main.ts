0e1e202d421c647c482fc1b8326d7d3df2d89aca
Seth Holloway <seth.holloway@tkxs.com>
2025-04-09 09:47:41 -0400

    feat(Components): Adding theme provider back to component | Fixing icon sizes | Fixing Popover/Tooltip display

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,6 +2,7 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+
   addons: [
     '@storybook/addon-onboarding',
     '@storybook/addon-essentials',
@@ -10,10 +11,20 @@ const config: StorybookConfig = {
     '@storybook/addon-themes',
     '@storybook/addon-a11y',
   ],
+
   framework: {
     name: '@storybook/react-vite',
     options: {},
   },
+
   staticDirs: ['../src/static'],
+
+  docs: {
+    autodocs: true
+  },
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript'
+  }
 };
 export default config;
