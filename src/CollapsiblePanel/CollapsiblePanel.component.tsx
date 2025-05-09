390a630689ae6a7122a81ccf9b3f7cde1d087223
Nkalpana12 <169072711+Nkalpana12@users.noreply.github.com>
2025-05-09 14:58:16 -0400

    feat(CollapsiblePanel): Add Collapsible Panel component (#1169)
    * feat(CollapisblePanel): Add Collapsible Panel component
    
    * export component
    
    * remove background color

---
+++ src/CollapsiblePanel/CollapsiblePanel.component.tsx
@@ -0,0 +1,86 @@
+import React, { useState, ReactNode, useEffect } from "react";
+import styled from "styled-components";
+import { KeyboardArrowDownIcon, KeyboardArrowRightIcon } from '@icons';
+
+export type CollapsiblePanelProps = {
+  title: string;
+  endContent?: ReactNode;
+  children: ReactNode;
+  defaultOpen?: boolean;
+}
+
+const PanelContainer = styled.div`
+  font-family: ${(props: any) => props.theme.typography.fontFamily};
+  padding: 4px;
+`;
+
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 8px 8px;
+  cursor: pointer;
+`;
+
+const TitleGroup = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  font-weight: 600;
+  font-size: 18px;
+`;
+
+const EndContentWrapper = styled.div`
+  margin-left: auto;
+  display: flex;
+  align-items: center;
+`;
+
+const Content = styled.div`
+  padding: 0px 20px;
+  font-size: 14px;
+`;
+
+const ArrowDownIcon = styled(KeyboardArrowDownIcon)`
+  color: ${(props) => props.theme.colors.primary};
+`
+const ArrowRightIcon = styled(KeyboardArrowRightIcon)`
+  color: ${(props) => props.theme.colors.primary};
+`
+
+export const CollapsiblePanel: React.FC<CollapsiblePanelProps> = ({
+  title,
+  endContent,
+  children,
+  defaultOpen
+}) => {
+
+  const [isOpen, setIsOpen] = useState(defaultOpen);
+
+  useEffect(() => {
+    setIsOpen(defaultOpen);
+  }, [defaultOpen]);
+
+  const togglePanel = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  return (
+    <PanelContainer>
+      <Header onClick={togglePanel}>
+        <TitleGroup>
+          {isOpen ? <ArrowDownIcon width={24} height={24} /> : <ArrowRightIcon width={24} height={24}/>}
+          <span>{title}</span>
+        </TitleGroup>
+        {endContent && (
+          <EndContentWrapper onClick={(e) => e.stopPropagation()}>
+            {endContent}
+          </EndContentWrapper>
+        )}
+      </Header>
+      {isOpen && <Content>{children}</Content>}
+    </PanelContainer>
+  );
+};
+
+export default CollapsiblePanel;
