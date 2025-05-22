af292785250dd5b60b1bee33a5bb8174815ff723
Nkalpana12 <kalpana.nagan@telusagcg.com>
2025-05-22 14:14:40 -0400

    use defaultprops

--- src/Typography/TextDisplay/TextDisplay.component.tsx
+++ src/Typography/TextDisplay/TextDisplay.component.tsx
@@ -1,28 +1,33 @@
 import React from 'react';
-import styled, { ThemeProvider, DefaultTheme } from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
 import { Themes } from '@themes';
 
 export type TextDisplayProps = React.PropsWithChildren<{
   /**
-   * Font size of the text.
+   * Font size of text
+   *
    * @default '16px'
-   */
+   **/
   fontSize?: string;
-
   /**
-   * Font family of the text.
-   * @default theme.typography.fontFamily
-   */
-  fontFamily?: string;
-
+   * From theme provider
+   *
+   * @default defaultTheme
+   **/
+  theme?: any;
   /**
-   * Theme from styled-components ThemeProvider.
-   * @default Themes.canopyTheme
-   */
-  theme?: DefaultTheme;
+   * font family of Text 
+   *
+   * @default defaultTheme
+   **/
+  fontFamily?: string
 }>;
 
-const defaultTheme = Themes.canopyTheme;
+const defaultProps = {
+  fontSize: '16px',
+  fontFamily: Themes.canopyTheme.typography.fontFamily,
+  theme: Themes.canopyTheme,
+} satisfies Partial<TextDisplayProps>;
 
 const StyledSpan = styled.span<{
   fontSize: string;
@@ -32,15 +37,11 @@ const StyledSpan = styled.span<{
   font-size: ${({ fontSize }) => fontSize};
 `;
 
-export const TextDisplay: React.FC<TextDisplayProps> = ({
-  fontSize = '16px',
-  fontFamily = defaultTheme.typography.fontFamily,
-  theme = defaultTheme,
-  children,
-  ...rest
-}) => {
+export const TextDisplay = (props: TextDisplayProps) => {
+  const propsWithDefaults = { ...defaultProps, ...props };
+  const { theme, fontSize, fontFamily, children, ...rest } = propsWithDefaults;
   return (
-    <ThemeProvider theme={(outerTheme: DefaultTheme) => outerTheme || theme}>
+    <ThemeProvider theme={(outerTheme: any) => outerTheme || theme}>
       <StyledSpan fontSize={fontSize} fontFamily={fontFamily} {...rest}>
         {children}
       </StyledSpan>
