06fd0a500d68087a331e8f2cc745da65a9badad2
Nkalpana12 <169072711+Nkalpana12@users.noreply.github.com>
2025-05-26 12:11:31 -0400

    feat(Icons): Add outlined Delete Icon (#1175)

--- src/Icons/icons.tsx
+++ src/Icons/icons.tsx
@@ -622,6 +622,23 @@ export function AddIcon(props: SVGProps<SVGSVGElement>) {
   );
 }
 
+export function DeleteOutlineIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width="1em"
+      height="1em"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6zm2 0V9h8v10H8zM15.5 4l-1-1h-5l-1 1H5v2h14V4z"
+      />
+    </svg>
+  );
+}
+
 export function DeleteIcon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
