b94f9aa9ec3fb3848a60604b9f9cf56cc25a7750
Nkalpana12 <kalpana.nagan@telusagcg.com>
2025-10-15 10:16:07 -0400

    feat(Icons): Add Double arrow icons

--- src/Icons/icons.tsx
+++ src/Icons/icons.tsx
@@ -1306,3 +1306,49 @@ export const PulseIcon = (props: SVGProps<SVGSVGElement>) => {
     </svg>
   );
 };
+
+export function DoubleArrowUpIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width="1em"
+      height="1em"
+      {...props}
+    >
+      <g transform="rotate(-90 12 12)">
+        <path
+          fill="currentColor"
+          d="M5.7 6.71a.996.996 0 0 0 0 1.41L9.58 12L5.7 15.88a.996.996 0 1 0 1.41 1.41l4.59-4.59a.996.996 0 0 0 0-1.41L7.12 6.71c-.39-.39-1.03-.39-1.42 0"
+        />
+        <path
+          fill="currentColor"
+          d="M12.29 6.71a.996.996 0 0 0 0 1.41L16.17 12l-3.88 3.88a.996.996 0 1 0 1.41 1.41l4.59-4.59a.996.996 0 0 0 0-1.41L13.7 6.7c-.38-.38-1.02-.38-1.41.01"
+        />
+      </g>
+    </svg>
+  );
+}
+
+export function DoubleArrowDownIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width="1em"
+      height="1em"
+      {...props}
+    >
+      <g transform="rotate(90 12 12)">
+        <path
+          fill="currentColor"
+          d="M5.7 6.71a.996.996 0 0 0 0 1.41L9.58 12L5.7 15.88a.996.996 0 1 0 1.41 1.41l4.59-4.59a.996.996 0 0 0 0-1.41L7.12 6.71c-.39-.39-1.03-.39-1.42 0"
+        />
+        <path
+          fill="currentColor"
+          d="M12.29 6.71a.996.996 0 0 0 0 1.41L16.17 12l-3.88 3.88a.996.996 0 1 0 1.41 1.41l4.59-4.59a.996.996 0 0 0 0-1.41L13.7 6.7c-.38-.38-1.02-.38-1.41.01"
+        />
+      </g>
+    </svg>
+  );
+}
