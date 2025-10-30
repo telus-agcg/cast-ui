3a446ea17be62542e849425d348f9b5fa1ad712f
Nkalpana12 <169072711+Nkalpana12@users.noreply.github.com>
2025-10-30 12:26:45 -0700

    feat(Icons): Add Edit icon (#1212)

--- src/Icons/icons.tsx
+++ src/Icons/icons.tsx
@@ -1352,3 +1352,32 @@ export function DoubleArrowDownIcon(props: SVGProps<SVGSVGElement>) {
     </svg>
   );
 }
+
+export function EditIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width="1em"
+      height="1em"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinejoin="round"
+        strokeLinecap="round"
+        d="m19.3 8.925l-4.25-4.2l1.4-1.4q.575-.575 1.413-.575t1.412.575l1.4 1.4q.575.575.6 1.388t-.55 1.387z"
+      ></path>
+      <path
+        stroke="currentColor"
+        fill="none"
+        strokeWidth="1.5"
+        strokeLinejoin="round"
+        strokeLinecap="round"
+        d="M4 21q-.425 0-.713-.288T3 20v-2.825q0-.2.075-.388t.225-.337l10.3-10.3l4.25 4.25l-10.3 10.3q-.15.15-.337.225T6.825 21z"
+      ></path>
+    </svg>
+  );
+}
