ae402d0e365d0a9c18acb80a60e6963aeb0bcfbe
Seth Holloway <35776783+Kynazeras@users.noreply.github.com>
2025-11-10 13:52:44 -0500

    feat(Icons): Adds drag indicator icons (#1214)

--- src/Icons/icons.tsx
+++ src/Icons/icons.tsx
@@ -1381,3 +1381,20 @@ export function EditIcon(props: SVGProps<SVGSVGElement>) {
     </svg>
   );
 }
+
+export function DragIndicatorIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width="1em"
+      height="1em"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        d="M11 18c0 1.1-.9 2-2 2s-2-.9-2-2s.9-2 2-2s2 .9 2 2m-2-8c-1.1 0-2 .9-2 2s.9 2 2 2s2-.9 2-2s-.9-2-2-2m0-6c-1.1 0-2 .9-2 2s.9 2 2 2s2-.9 2-2s-.9-2-2-2m6 4c1.1 0 2-.9 2-2s-.9-2-2-2s-2 .9-2 2s.9 2 2 2m0 2c-1.1 0-2 .9-2 2s.9 2 2 2s2-.9 2-2s-.9-2-2-2m0 6c-1.1 0-2 .9-2 2s.9 2 2 2s2-.9 2-2s-.9-2-2-2"
+      ></path>
+    </svg>
+  );
+}
