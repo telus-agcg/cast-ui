62b21b92feb85c09f885a997355d91d3bec6fd93
Sachin LNU <sachin.lnu02@telusinternational.com>
2026-06-11 15:06:45 +0530

    fix(DatePicker): fix wrapper not filling parent container in flex layouts

--- src/DatePicker/DatePicker.component.tsx
+++ src/DatePicker/DatePicker.component.tsx
@@ -138,7 +138,7 @@ const SWrapperComponent = styled.div<{
   showIcon: boolean;
 }>`
   position: relative;
-  font-family: ${(props) => props.theme.typography.fontFamily};
+    font-family: ${(props) => props.theme.typography.fontFamily};
   font-size: ${(props) => props.theme.common[props.datePickerSize!].fontSize};
   color: ${(props) => props.theme.styles[props.datePickerStyle!].text};
   input {
@@ -156,6 +156,15 @@ const SWrapperComponent = styled.div<{
       props.invalid ? props.theme.validation.borderColor : ''};
     cursor: pointer;
   }
+.react-datepicker-wrapper {
+  display: block;
+  width: 100%;
+}
+
+.react-datepicker__input-container {
+  display: block;
+  width: 100%;
+}
 
   .react-datepicker {
     display: flex;
